53dc9ff04176b40a728043ff98a96fddd0e7edb1
sante <santes.au@gmail.com>
2023-10-19 09:20:55 +0300

    5

---
+++ easy/modifySalaryColumn.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    new_emp = employees.assign(salary = employees['salary'] * 2)
+    return new_emp
+
+
+data = pd.DataFrame({
+    "name": ['Jack', 'Piper', 'Mia', 'Ulysses'],
+
+    "salary": [19666, 74754, 62509, 54866],
+}
+)
+
+new_temp = modifySalaryColumn(data)
+print(f'"{new_temp}"')
