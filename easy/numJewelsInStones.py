4be96386f653c9588662c76e5fb7c8307a39db99
sante <santes.au@gmail.com>
2023-10-19 10:29:35 +0300

    6

---
+++ easy/numJewelsInStones.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        """Количество вхождение камней"""
+        sum = 0
+        for i in jewels:
+            x = stones.count(i)
+            sum = sum +x
+
+
+        return sum
+
+sol = Solution()
+number = sol.numJewelsInStones('z', 'ZZ')
+print(number)
+
