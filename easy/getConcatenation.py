6ee933d6afc097b922a1921168691d39def6096f
Sandi_Navoiy <santes.au@gmail.com>
2023-10-28 12:26:47 +0300

    8

---
+++ easy/getConcatenation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        """удваение списка"""
+
+        nums_new = nums *2
+        return nums_new
+
+nums = [1,3,2,1]
+
+sol = Solution()
+
+print(sol.getConcatenation(nums))
