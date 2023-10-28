a429619e5b25ad8e4f3f3072b18d25bdcce7384c
Sandi_Navoiy <santes.au@gmail.com>
2023-10-28 15:29:20 +0300

    8

---
+++ easy/maximumWealth.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        max_list = []
+
+        for i in accounts:
+            sum_of_list = sum(i)
+            max_list.append(sum_of_list)
+        return max(max_list)
+
+accaunts = [[1,5],[7,3],[3,5]]
+
+s = Solution()
+print(s.maximumWealth(accaunts))
