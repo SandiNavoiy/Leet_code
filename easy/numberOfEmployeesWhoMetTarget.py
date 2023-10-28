5910da4b0db2b84a28f0e0b71f677b41e51f8821
Sandi_Navoiy <santes.au@gmail.com>
2023-10-28 16:08:30 +0300

    9

---
+++ easy/numberOfEmployeesWhoMetTarget.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: list[int], target: int) -> int:
+        sum = 0
+        for i in hours:
+            if i >= target:
+                sum += 1
+
+        return sum
+
+
+
+hours = [0,1,2,3,4]
+target = 2
+
+s  = Solution()
+print(s.numberOfEmployeesWhoMetTarget(hours, target))
