956f7508779040bb75ab6fde9455512d48d3c617
Sandi_Navoiy <santes.au@gmail.com>
2023-11-01 22:51:12 +0300

    27

---
+++ easy/minimumOperations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumOperations(self, nums: list[int]) -> int:
+        """Сделать массив нулевым, вычитая равные суммы"""
+        wer = []
+        for i in nums:
+            if i != 0:
+                wer.append(i)
+        iter = len(set(wer))
+        return iter
+
+
+
+nums = [1,5,0,3,5]
+s = Solution()
+print(s.minimumOperations(nums))
