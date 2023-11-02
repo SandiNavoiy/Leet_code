4c79a489ff220b3aa1e68eb4e828bd60b18c035e
Sandi_Navoiy <santes.au@gmail.com>
2023-11-02 21:27:53 +0300

    34

---
+++ easy/1662.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: list[str], word2: list[str]) -> bool:
+        """Проверьте, эквивалентны ли два массива строк"""
+        w  = "".join(word1)
+        e = "".join(word2)
+        if w == e:
+            return True
+        else:
+            return False
+
+
+
+word1 = ["ab", "c"]
+word2 = ["a", "bc"]
+s = Solution()
+print(s.arrayStringsAreEqual(word1, word2))
