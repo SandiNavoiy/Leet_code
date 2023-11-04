87b6be7dbd305eddeef8484eba42ddcd23cc6528
Sandi_Navoiy <santes.au@gmail.com>
2023-11-04 17:15:46 +0300

    39

---
+++ easy/1389.py
@@ -0,0 +1,13 @@
+class Solution:
+    def createTargetArray(self, nums: list[int], index: list[int]) -> list[int]:
+        """Создайте целевой массив в заданном порядке"""
+        new = []
+        for i in range(len(index)):
+            new.insert(index[i], nums[i])
+
+        return new
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+
+s = Solution()
+print(s.createTargetArray(nums, index))
---
+++ easy/in_work/1221.py
@@ -0,0 +1,10 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        """Разделение струны в сбалансированных струнах"""
+        r_val = []
+        l_val = []
+
+
+s = 'RLRRLLRLRL'
+sol = Solution()
+print(sol.balancedStringSplit(s))
