ed40149f8c3764ab05fb83944a998734f913fa0a
Sandi_Navoiy <santes.au@gmail.com>
2023-11-04 14:19:05 +0300

    36

---
+++ easy/1791.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findCenter(self, edges: list[list[int]]) -> int:
+        """Найти центр звездного графика"""
+        new  = []
+        res = 0
+        for i in edges:
+            for j in i:
+                new.append(j)
+        for i in new:
+            if new.count(i) > 1:
+                res = i
+
+        return res
+edges = [[1, 2], [2, 3], [4, 2]]
+s = Solution()
+print(s.findCenter(edges))
---
+++ easy/1832.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        """Проверьте, является ли предложение панграммой"""
+        alf = "abcdefghijklmnopqrstuvwxyz"
+        sum = 0
+        for i in  alf:
+            if i in sentence:
+                sum += 1
+
+        print(sum)
+        if sum == 26:
+            return True
+        else:
+            return False
+
+
+
+
+
+sentence = "thequickbrownfoxjumpsoverthelazydog"
+s = Solution()
+print(s.checkIfPangram(sentence))
