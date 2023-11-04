57de2520ff9735d94fea63677214a47b39ef9a73
Sandi_Navoiy <santes.au@gmail.com>
2023-11-05 00:10:36 +0300

    44

---
+++ easy/2810.py
@@ -0,0 +1,17 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        """Неисправная клавиатура"""
+        new_str = ""
+        for i in s:
+            if i != "i":
+                new_str = new_str + i
+            else:
+                new_str = ''.join(reversed(new_str))
+        return new_str
+
+
+
+
+s = "string"
+sol = Solution()
+print(sol.finalString(s))
