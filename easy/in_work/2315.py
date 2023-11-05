b212f5a61e895d6ead739892b565cf5f049e8df6
Sandi_Navoiy <santes.au@gmail.com>
2023-11-05 20:46:22 +0300

    48

---
+++ easy/in_work/2315.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        """Подсчитайте звездочки"""
+        v = 0
+        sum = 0
+        for i in range(len(s)):
+            if s[i] == "|":
+                v = v + 1
+            if v % 2 == 0:
+                if s[i] == "*":
+                    sum = sum + 1
+        return sum
+
+s = "l|*e*et|c**o|*de|"
+
+sol =  Solution()
+print(sol.countAsterisks(s))
