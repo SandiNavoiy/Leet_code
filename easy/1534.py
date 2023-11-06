baef22a07df9015d43b0ab50b4ca2ab8ea5ca439
Sandi_Navoiy <santes.au@gmail.com>
2023-11-06 12:29:34 +0300

    50

---
+++ easy/1534.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countGoodTriplets(self, arr: list[int], a: int, b: int, c: int) -> int:
+        count = 0
+        for i in range(len(arr) - 2):
+            for j in range(i + 1, len(arr) - 1):
+                for k in range(j + 1, len(arr)):
+                        if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c and abs(arr[i] - arr[j]) <= a:
+                            count += 1
+        return count
+
+arr = [3,0,1,1,9,7]
+a = 7
+b = 2
+c = 3
+
+s = Solution()
+print(s.countGoodTriplets(arr, a, b, c))
---
+++ easy/2103.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        '''Кольца и стержни'''
+        sunn = 0
+        for i in range(len(rings) - 1):
+           i = str(i)
+           if "R" + i in rings and "B" + i in rings and "G" + i in rings:
+                sunn = sunn + 1
+
+        return sunn
+
+
+rings = "B0B6G0R6R0R6G9"
+
+s = Solution()
+print(s.countPoints(rings))
