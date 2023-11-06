0bb83b2c1410b38873018aee81a35b245ff77745
Sandi_Navoiy <santes.au@gmail.com>
2023-11-06 20:42:00 +0300

    60

---
+++ medium/46.py
@@ -0,0 +1,13 @@
+import itertools
+
+class Solution:
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        perm_set = itertools.permutations(nums)
+        new = []
+        for i in perm_set:
+            new.append(list(i))
+        return new
+nums = [1, 2, 3]
+
+s = Solution()
+print(s.permute(nums))
