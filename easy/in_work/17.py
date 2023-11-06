398e92fffbabffb53d6219f00ed8b210da40c893
Sandi_Navoiy <santes.au@gmail.com>
2023-11-06 20:09:02 +0300

    58

--- easy/in_work/17.py
+++ easy/in_work/17.py
@@ -1,15 +1,20 @@
+import itertools
 class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
         """Буквенные комбинации номера телефона"""
         roman_numbers = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl",
                          '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
+        if digits =="":
+            return []
 
         new_list = []
         for i in digits:
             new_list.append(roman_numbers[i])
+        combinations = list(itertools.product(*new_list))
+        result_list = [''.join(combination) for combination in combinations]
 
-        return new_list
+        return result_list
 
-digits = "29"
+digits = ""
 s = Solution()
 print(s.letterCombinations(digits))
