0c92a1805033a99ee02c6b48ac15f3f84a4250e9
Sandi_Navoiy <santes.au@gmail.com>
2023-11-08 18:06:10 +0300

    73

---
+++ easy/27.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        """"""
+        while val in nums:
+            nums.remove(val)
+
+        return len(nums)
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+s = Solution()
+print(s.removeElement(nums, val))
+nums = [0,1,2,2,3,0,4,2]
