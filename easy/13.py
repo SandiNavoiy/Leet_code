dba2e164ff9c7b2550459ed8d0d677ad3e231a8a
Sandi_Navoiy <santes.au@gmail.com>
2023-11-08 17:28:08 +0300

    72

---
+++ easy/13.py
@@ -0,0 +1,33 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        """Роман в целое число"""
+        s = s.replace("CM", "DCCCC").replace("CD", "CCCC").replace("XC", "LXXXX").replace("XL", "XXXX").replace("IX", "VIIII").replace("IV", "IIII")
+
+
+        roman_numbers = {
+            "M": 1000,
+            "CM": 900,
+            "D": 500,
+            "CD": 400,
+            "C": 100,
+            "XC": 90,
+            "L": 50,
+            "XL": 40,
+            "X": 10,
+            "IX": 9,
+            "V": 5,
+            "IV": 4,
+            "I": 1,
+        }
+        sum  = 0
+        for i in s:
+
+            sum = sum + roman_numbers[i]
+
+        return sum
+
+s = "MCMXCIV"
+#1994
+sol =Solution()
+print(sol.romanToInt(s))
+
---
+++ easy/28.py
@@ -0,0 +1,13 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        """"""
+
+        i = haystack.find(needle)
+        return i
+
+
+haystack = "leetcode"
+Needle = "leeto"
+
+s = Solution()
+print(s.strStr(haystack, Needle))
