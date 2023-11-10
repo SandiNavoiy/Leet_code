3b8149a47b3ed29befdfd0353e9ee9d300fab271
Sandi_Navoiy <santes.au@gmail.com>
2023-11-10 21:58:10 +0300

    76

---
+++ medium/2161.py
@@ -0,0 +1,22 @@
+class Solution:
+    def pivotArray(self, nums: list[int], pivot: int) -> list[int]:
+        """"Разделение массива в соответствии с заданным центром"""
+        new_min = []
+        new_max = []
+        new_ser = []
+
+        for i in range(len(nums)):
+            if nums[i] < pivot:
+                new_min.append(nums[i])
+            elif nums[i] > pivot:
+                new_max.append(nums[i])
+            elif nums[i] == pivot:
+                new_ser.append(nums[i])
+        return new_min + new_ser + new_max
+
+
+nums = [9,12,5,10,14,3,10]
+pivot = 10
+#[9,5,3,10,10,12,14]
+s = Solution()
+print(s.pivotArray(nums, pivot))
