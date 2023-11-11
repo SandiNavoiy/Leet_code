796ee78e3aecef2b2c086d110e7303af51ef1d15
Sandi_Navoiy <santes.au@gmail.com>
2023-11-11 16:36:13 +0300

    78

---
+++ medium/in_work/2610.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+class Solution:
+    def findMatrix(self, nums: list[int]) -> list[list[int]]:
+        output = []
+
+        while len(nums):
+            output.append(list(set(nums)))
+
+            for i in output[-1]:
+                nums.pop(nums.index(i))
+
+        return output
+
+nums = [1,3,4,1,2,3,1]
+s = Solution()
+print(s.findMatrix(nums))
+#[[1,3,4,2],[1,3],[1]]
