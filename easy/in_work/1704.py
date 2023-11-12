41147c262546eb3a0b7cd12454da0d830da54d5f
Sandi_Navoiy <santes.au@gmail.com>
2023-11-12 16:05:03 +0300

    92

---
+++ easy/in_work/1704.py
@@ -0,0 +1,23 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        """Определите, похожи ли половинки струны"""
+        w = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+        l = len(s)
+
+        left = []
+        right = []
+        for i in s[0: int(l / 2)]:
+            if i in w:
+                left.append(i)
+        for j in s[int(l / 2):]:
+            print(j)
+            if j in w:
+                right.append(j)
+
+        return (len(left) == len(right))
+
+
+s = "AbCdEfGh"
+
+sol = Solution()
+print(sol.halvesAreAlike(s))
