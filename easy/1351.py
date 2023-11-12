6df824f5253562e4d3f6f233f0ea5696b162b595
Sandi_Navoiy <santes.au@gmail.com>
2023-11-12 16:11:54 +0300

    93

---
+++ easy/1351.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countNegatives(self, grid: list[list[int]]) -> int:
+        """Подсчитайте отрицательные числа в отсортированной матрице"""
+        itern = 0
+        for i in grid:
+            for j in i:
+                if j < 0:
+                    itern += 1
+        return itern
+
+grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,- 3]]
+s = Solution()
+print(s.countNegatives(grid))
