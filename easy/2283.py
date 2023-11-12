48ac3562cb6b65ceace1ce0a2e5ca05fcccc4a43
Sandi_Navoiy <santes.au@gmail.com>
2023-11-12 22:51:56 +0300

    97

---
+++ easy/2283.py
@@ -0,0 +1,18 @@
+class Solution:
+    def digitCount(self, num: str) -> bool:
+        """ . Проверьте, имеет ли число одинаковое количество цифр и значение цифры"""
+        ee = []
+        for i in range(len(num)):
+            print(num.count(str(i)))
+            print(num[i])
+            print("----------")
+            if num.count(str(i)) == int(num[i]):
+                ee.append(True)
+            else:
+                ee.append(False)
+                return False
+        return ee
+
+num = "030"
+s = Solution()
+print(s.digitCount(num))
