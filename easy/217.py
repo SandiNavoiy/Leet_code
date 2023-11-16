1e42dcb6735a3e83daf48fe9a829259ea58de253
Sandi_Navoiy <santes.au@gmail.com>
2023-11-16 22:18:58 +0300

    110

---
+++ easy/217.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        """Содержит дубликат"""
+        if len(nums) == len(set(nums)):
+            return False
+        else:
+            return True
+
+nums = [1,2,3,1]
+s = Solution()
+print(s.containsDuplicate(nums))
+
+
+
---
+++ easy/in_work/202.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        """Счастливое число"""
+        new = str(n)
+        for i in new:
+            pass
+        return new
+n = 19
+s = Solution()
+print(s.isHappy(n))
