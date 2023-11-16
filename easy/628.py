7fc47474f698937469698f1bcd1767c47d34114d
Sandi_Navoiy <santes.au@gmail.com>
2023-11-16 20:41:45 +0300

    108

---
+++ easy/628.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumProduct(self, nums: list[int]) -> int:
+        """Максимальное произведение трех чисел"""
+        nums.sort()
+        print(nums)
+        return max(nums[-1] * nums[-2] * nums[-3], nums[-1] * nums[1] * nums[0])
+
+
+nums = [-100,-98,-1,2,3,4]
+s = Solution()
+print(s.maximumProduct(nums))
