19e20b634c856a439773fe723948aebc99cba5f8
sante <santes.au@gmail.com>
2023-11-16 10:15:51 +0300

    104

---
+++ easy/1502.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: list[int]) -> bool:
+        """Может сделать арифметическую прогрессию из последовательности"""
+        arr.sort()
+        list_chek = []
+        for i in range(0, len(arr)-2):
+            if arr[i]-arr[i+1] == arr[i+1] - arr[i+2]:
+                list_chek.append('True')
+            else:
+                list_chek.append('False')
+
+        if 'False' in list_chek:
+            return False
+        else:
+            return True
+
+
+
+arr = [1,2,4]
+
+s = Solution()
+print(s.canMakeArithmeticProgression(arr))
+
---
+++ easy/in_work/2085.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countWords(self, words1: list[str], words2: list[str]) -> int:
+        """"""
+        ss = 0
+        for i in words1:
+            if i in words2 and words1.count(i) == 1:
+                ss = ss + 1
+        return ss
+
+word1 = ["a","ab"]
+words2 = ["a","a","a","ab"]
+
+s = Solution()
+print(s.countWords(word1, words2))
+
