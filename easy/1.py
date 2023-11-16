a3b181640a21f57336df17e982c0ce0f14724238
Sandi_Navoiy <santes.au@gmail.com>
2023-11-16 20:58:19 +0300

    108

---
+++ easy/1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        """Две суммы"""
+        new = []
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    new.append(i)
+                    new.append(j)
+        return list(set(new))
+
+nums = [3,2,4]
+target = 6
+s = Solution()
+print(s.twoSum(nums, target))
