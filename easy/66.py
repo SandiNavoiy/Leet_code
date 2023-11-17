67514621a8e40e1e4e439eea18fa681b2287f84a
Sandi_Navoiy <santes.au@gmail.com>
2023-11-17 20:22:33 +0300

    115

---
+++ easy/66.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        """Плюс один"""
+        new = int("".join(map(str, digits)))
+        new = str(new + 1)
+        n = []
+        for i in new:
+            n.append(int(i))
+
+        return n
+digits = [1,2,3]
+s = Solution()
+print(s.plusOne(digits))
