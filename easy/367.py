e8ceca473c984f2641c425baaa55da7444ba842b
Sandi_Navoiy <santes.au@gmail.com>
2023-11-18 22:12:47 +0300

    122

---
+++ easy/367.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        """"""
+        x = num ** 0.5
+        if int(x) == x:
+            return True
+        else:
+            return False
+num = 15
+
+s = Solution()
+print(s.isPerfectSquare(num))
