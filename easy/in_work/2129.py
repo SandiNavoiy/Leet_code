73b0e908e1dcac12c24d6ccc52190ecfdcc4adfa
Sandi_Navoiy <santes.au@gmail.com>
2023-11-19 13:52:16 +0300

    126

---
+++ easy/in_work/2129.py
@@ -0,0 +1,14 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        """Назовите название с заглавной буквы"""
+        arr = title.split()
+
+        for i in range(len(arr)):
+            arr[i] = arr[i].lower()
+            if len(arr[i]) >= 3:
+                arr[i] = arr[i].title()
+        return " ".join(arr)
+
+title = "capiTalIze tHe titLe"
+s = Solution()
+print(s.capitalizeTitle(title))
