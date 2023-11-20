b3fbc2fc1e0b4b09a8180f77b5b5cb14964f4004
sante <santes.au@gmail.com>
2023-11-20 09:35:03 +0300

    127

---
+++ easy/169.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        """"""
+        n = 0
+        m = 0
+
+        for i in nums:
+
+            if nums.count(i) > n:
+                n = nums.count(i)
+                m = i
+        return m
+
+
+
+
+nums = [3,2,3]
+s = Solution()
+print(s.majorityElement(nums))
