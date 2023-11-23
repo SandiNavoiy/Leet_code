ebf37d07f3c01c948016ce142553029a5a9b87a5
sante <santes.au@gmail.com>
2023-11-23 10:08:08 +0300

    133

---
+++ easy/in_work/1408.py
@@ -0,0 +1,14 @@
+class Solution:
+    def stringMatching(self, words: list[str]) -> list[str]:
+        """"""
+        new = []
+        for i in words:
+            for j in words:
+                if j in i and j != i:
+                    new.append(j)
+
+        return list(set(new))
+word = ["leetcoder","leetcode","od","hamlet","am"]
+
+s = Solution()
+print(s.stringMatching(word))
