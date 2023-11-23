51b7b6e5178281cc0a7b7f4ad0b8aa26e32ce5b7
sante <santes.au@gmail.com>
2023-11-23 10:16:01 +0300

    134

---
+++ easy/1491.py
@@ -0,0 +1,13 @@
+class Solution:
+    def average(self, salary: list[int]) -> float:
+        """Средняя зарплата без учета минимальной и максимальной зарплаты"""
+
+        salary.sort()
+
+        x = sum(salary[1:-1]) / len(salary[1:-1])
+        return x
+
+salary = [4000,3000,1000,2000]
+
+s = Solution()
+print(s.average(salary))
