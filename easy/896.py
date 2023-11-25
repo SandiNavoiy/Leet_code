c24ce98ae650c958ea3db754c7e382a56b72fd68
Sandi_Navoiy <santes.au@gmail.com>
2023-11-25 15:03:09 +0300

    138

---
+++ easy/896.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isMonotonic(self, nums: list[int]) -> bool:
+        """монотонный массив"""
+
+        if (all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1))):
+            return True
+
+        elif (all(nums[i] >= nums[i + 1] for i in range(len(nums) - 1))):
+            return True
+        else:
+            return False
+
+
+nums = [6, 5, 4, 4]
+s = Solution()
+print(s.isMonotonic(nums))
