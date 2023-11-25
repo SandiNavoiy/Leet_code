78faaa0dc2b82aada2710e5940a5fc7efc655a83
Sandi_Navoiy <santes.au@gmail.com>
2023-11-25 15:51:49 +0300

    140

---
+++ easy/2465.py
@@ -0,0 +1,11 @@
+class Solution:
+    def distinctAverages(self, nums: list[int]) -> int:
+        """Количество различных средних значений"""
+        nums.sort()
+        unique_averages = set()
+        while len(nums) > 1:
+                  unique_averages.add((nums.pop(-1) + nums.pop(0)) / 2)
+        return len(set(unique_averages))
+nums = [4,1,4,0,3,5]
+s= Solution()
+print(s.distinctAverages(nums))
