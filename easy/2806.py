4dbb78a15b99c61c7cb8eed18b02745ce77d1c9c
Sandi_Navoiy <santes.au@gmail.com>
2023-11-26 16:36:12 +0300

    142

---
+++ easy/2806.py
@@ -0,0 +1,16 @@
+class Solution:
+    def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
+        amm = 100
+        temp = (amm - purchaseAmount) // 10
+        if (amm - purchaseAmount) % 10 <= 5:
+            return  temp * 10
+        else:
+            return (temp + 1) * 10
+
+
+
+
+purchaseAmount = 15
+
+s = Solution()
+print(s.accountBalanceAfterPurchase(purchaseAmount))
