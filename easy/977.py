882e4f4d95248a80dedbae42d85640e0ab69bfe8
Sandi_Navoiy <santes.au@gmail.com>
2023-11-26 22:45:39 +0300

    145

---
+++ easy/977.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortedSquares(self, nums: list[int]) -> list[int]:
+        """Квадраты отсортированного массива"""
+        new = []
+        for i in nums:
+            new.append(i*i)
+        new.sort()
+        return new
+nums = [-4,-1,0,3,10]
+s= Solution()
+print(s.sortedSquares(nums))
