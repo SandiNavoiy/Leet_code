c0c413389b33e5a6634347bcf3f86fee20831a1c
sante <santes.au@gmail.com>
2023-11-27 09:30:11 +0300

    146

---
+++ easy/342.py
@@ -0,0 +1,13 @@
+from math import log
+
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        """Сила четырех"""
+        if n<=0:
+            return False
+        return n>0 and log(n,4).is_integer()
+
+n = 4
+s = Solution()
+print(s.isPowerOfFour(n))
