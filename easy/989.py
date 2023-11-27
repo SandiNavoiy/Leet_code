6d9127f555545635c9f2d8b69e03fad5795b5c5e
Sandi_Navoiy <santes.au@gmail.com>
2023-11-27 15:08:49 +0300

    149

---
+++ easy/989.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addToArrayForm(self, num: list[int], k: int) -> list[int]:
+        """Добавить в массив целых чисел"""
+        x = "".join(map(str, num))
+        try:
+            new = int(x) + k
+            y = str(new)
+        except OverflowError:
+            raise ValueError("Result exceeds the limit for integer string conversion.")
+
+        ss = []
+        for i in y:
+           ss.append(int(i))
+
+        return ss
+
+num = [1, 2, 0, 0]
+k = 34
+s = Solution()
+print(s.addToArrayForm(num, k))
