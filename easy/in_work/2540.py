be0c3f13bdd98c0b7c975507897f3fa41225801f
sante <santes.au@gmail.com>
2023-11-28 12:56:46 +0300

    150

--- easy/in_work/2540.py
+++ easy/in_work/2540.py
@@ -2,10 +2,12 @@ class Solution:
     def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
         """"""
         x = set(nums1)&set(nums2)
+        if len(x) == 0:
+            return -1
         return min(x)
 
 
-nums1 = [1,2,3]
+nums1 = [1,3,3]
 nums2 = [2,4]
 s = Solution()
 print(s.getCommon(nums1, nums2))
