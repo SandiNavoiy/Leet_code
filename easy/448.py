cff2350fbfc71ab063b1fd3b9ff38fa35c7be341
Sandi_Navoiy <santes.au@gmail.com>
2023-12-02 13:10:10 +0300

    153

---
+++ easy/448.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        """Найти все числа, исчезнувшие в массиве"""
+        new = []
+        for i in range(1, len(nums) +1 ):
+            if i not in set(nums):
+                new.append(i)
+        return new
+
+
+
+nums = [4,3,2,7,8,2,3,1]
+s = Solution()
+print(s.findDisappearedNumbers(nums))
