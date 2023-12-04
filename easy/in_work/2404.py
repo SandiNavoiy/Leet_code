501bd7b63fe84c1004d3da20f3836292ab695df1
Sandi_Navoiy <santes.au@gmail.com>
2023-12-04 22:43:22 +0300

    +

---
+++ easy/in_work/2404.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mostFrequentEven(self, nums: list[int]) -> int:
+        """"""
+        if len(nums) == 1:
+            return nums[0]
+        x = 1
+        y = []
+        for i in nums:
+
+            if nums.count(i) > x and i  % 2 == 0:
+
+                x  = nums.count(i)
+
+                y.append(i)
+        if x == 1:
+            return -1
+        else:
+            return y
+
+nums = [2,10000,10000,10000,2]
+s = Solution()
+print(s.mostFrequentEven(nums))
