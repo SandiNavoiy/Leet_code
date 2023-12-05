085c4d5f845a8cf1967fcf5240f23a400c56bfe4
Sandi_Navoiy <santes.au@gmail.com>
2023-12-05 20:18:01 +0300

    169

---
+++ easy/2401.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxK(self, nums: list[int]) -> int:
+       """"""
+       negat = []
+       pozit = []
+
+       for i in nums:
+           if i < 0:
+               negat.append(abs(i))
+           else:
+               pozit.append(i)
+       x = set(negat) & set(pozit)
+       if len(x) == 0:
+           return -1
+       else:
+           return max(x)
+
+nums = [-10,8,6,7,-2,-3]
+s = Solution()
+print(s.findMaxK(nums))
