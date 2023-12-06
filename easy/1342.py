e55c2229cc6c01467e3167ed03d1f622d49b2db4
Sandi_Navoiy <santes.au@gmail.com>
2023-12-06 22:16:43 +0300

    170

---
+++ easy/1342.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        """"""
+        x = 0
+        while num > 0:
+            x += 1
+            if num % 2 == 0:
+                num = num / 2
+
+            else:
+                num -= 1
+
+        return x
+
+num = 14
+s = Solution()
+print(s.numberOfSteps(num))
