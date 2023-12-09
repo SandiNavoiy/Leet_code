ee79e85a621f6e20d98fa79c4faaef99efe0a72b
Sandi_Navoiy <santes.au@gmail.com>
2023-12-09 22:06:35 +0300

    175

---
+++ easy/2148.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countElements(self, nums: list[int]) -> int:
+        """"""
+        x = len(nums) - nums.count(max(nums)) - nums.count(min(nums))
+        if x <= 0:
+            return 0
+
+        return x
+
+nums = [-71,-71,93,-71,40]
+s = Solution()
+print(s.countElements(nums))
