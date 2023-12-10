5a75cf319cc96331da119141dd769fcf70dcb7d7
Sandi_Navoiy <santes.au@gmail.com>
2023-12-10 16:47:32 +0300

    179

---
+++ easy/350.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        """Пересечение двух массивов II"""
+        counter1 = Counter(nums1)
+        counter2 = Counter(nums2)
+        print(counter1)
+
+        intersection = []
+        for num, count in counter1.items():
+            common_count = min(count, counter2.get(num, 0))
+            intersection.extend([num] * common_count)
+
+        return intersection
+
+nums1 = [3,1,2]
+nums2 = [1,1]
+s= Solution()
+print(s.intersect(nums1, nums2))
