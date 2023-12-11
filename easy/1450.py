06c6068e6297c4fe061ae6815ea1dec6582fed7a
Sandi_Navoiy <santes.au@gmail.com>
2023-12-11 17:05:25 +0300

    184

---
+++ easy/1450.py
@@ -0,0 +1,14 @@
+class Solution:
+    def busyStudent(self, startTime: list[int], endTime: list[int], queryTime: int) -> int:
+        """"""
+        x = 0
+        for i in range(max(len(startTime), len(endTime))) :
+            if startTime[i] <= queryTime and endTime[i] >= queryTime:
+                x += 1
+        return x
+
+startTime = [1,2,3]
+endTime = [3,2,7]
+queryTime = 4
+s = Solution()
+print(s.busyStudent(startTime, endTime, queryTime))
