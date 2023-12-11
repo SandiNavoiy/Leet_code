f6b588bb12042cd33744b3a1807ce2977e5fa945
Sandi_Navoiy <santes.au@gmail.com>
2023-12-11 22:07:54 +0300

    188

---
+++ easy/2255.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPrefixes(self, words: list[str], s: str) -> int:
+        """"""
+        x = 0
+        for i in words:
+            if s.startswith(i):
+                x +=  1
+
+        return x
+
+words = ["a","b","c","ab","bc","abc"]
+s = "abc"
+sol = Solution()
+print(sol.countPrefixes(words, s))
---
+++ easy/2788.py
@@ -0,0 +1,19 @@
+class Solution:
+    def splitWordsBySeparator(self, words: list[str], separator: str) -> list[str]:
+        """"""
+        new = []
+        for i in words:
+            if separator in i:
+                x = i.split(separator)
+                new.extend(x)
+            else:
+                new.append(i)
+        while '' in new:
+            new.remove('')
+        return new
+
+words = ["$easy$","$problem$"]
+separator = "$"
+# ["one","two","three","four","five","six"]
+s = Solution()
+print(s.splitWordsBySeparator(words, separator))
