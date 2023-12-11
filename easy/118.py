213dcfe746dfbb2a8543cfe53ceb21a5128cdd97
Sandi_Navoiy <santes.au@gmail.com>
2023-12-11 17:31:21 +0300

    186

---
+++ easy/118.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        """"""
+        row = [1]
+        for i in range(numRows):
+            print(row)
+            row = [sum(x) for x in zip([0] + row, row + [0])]
+        return row
+
+
+
+numRows = 5
+s = Solution()
+print(s.generate(numRows))
