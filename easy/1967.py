406fc83cd8a43e31ef5b81ed7bce3f442163fc4d
Sandi_Navoiy <santes.au@gmail.com>
2023-12-11 16:54:06 +0300

    183

---
+++ easy/1967.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numOfStrings(self, patterns: list[str], word: str) -> int:
+        """"""
+        x = 0
+        for i in patterns:
+            if i in word:
+
+                x += 1
+        return x
+
+
+patterns = ["a", "abc", "bc", "d"]
+word = "abc"
+s = Solution()
+print(s.numOfStrings(patterns, word))
---
+++ easy/2744.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumNumberOfStringPairs(self, words: list[str]) -> int:
+        """"""
+        x = 0
+        for i in words:
+            for j in words:
+                if i == j[::-1] and i != j:
+                    print(i)
+
+                    x +=1
+        return int(x/2)
+
+words = ["ff", "tx", "qr", "zw", "wr", "jr", "zt", "jk", "sq", "xx" ]
+s = Solution()
+print(s.maximumNumberOfStringPairs(words))
