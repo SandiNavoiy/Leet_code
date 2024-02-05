1091b1d24c27d0cdbe388fd0ead6cf0a99eb6f8f
Sandi_Navoiy <santes.au@gmail.com>
2024-02-05 18:25:09 +0300

    204

---
+++ easy/2859.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: list[int], k: int) -> int:
+        """"""
+        new = 0
+        for i in range(len(nums)):
+            ind = format(i, 'b')
+            if ind.count("1") == k:
+                new += nums[i]
+        return new
+
+
+nums = [5,10,1,5,2]
+k = 1
+s = Solution()
+print(s.sumIndicesWithKSetBits(nums, k))
