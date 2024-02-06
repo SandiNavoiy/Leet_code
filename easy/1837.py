b7b5719e3094583e2b015e63a7e96cdccfaef880
Sandi_Navoiy <santes.au@gmail.com>
2024-02-06 09:41:31 +0300

    210

---
+++ easy/1837.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        ''''''
+        summ = 0
+        while n:
+            summ += n % k
+            n //= k
+        return summ
+n = 34
+k = 6
+s = Solution()
+print(s.sumBase(n, k))
