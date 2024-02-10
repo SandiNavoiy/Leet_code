06920905315372b63ac4027291876972f620112c
Sandi_Navoiy <santes.au@gmail.com>
2024-02-10 11:51:57 +0300

    222

---
+++ easy/504.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        ''''''
+        is_negative = False
+        if num == 0:
+            return str(num)
+        result = ""
+        if num < 0:
+            is_negative = True
+            num = abs(num)
+        while num > 0:
+            remainder = num % 7
+            result = str(remainder) + result
+            num //= 7
+        if is_negative:
+            result = "-" +  result
+        return result
+
+num = 100
+s = Solution()
+print(s.convertToBase7(num))
