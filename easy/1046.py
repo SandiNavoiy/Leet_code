69892d13222f7dd62ab947e183cc0fb1f9b81157
Sandi_Navoiy <santes.au@gmail.com>
2024-02-12 15:31:37 +0300

    226

---
+++ easy/1046.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lastStoneWeight(self, stones: list[int]) -> int:
+        ''''''
+        while len(stones) > 2:
+            stones.sort()
+            print(stones)
+            x = stones[-1]
+            y = stones[-2]
+            if x != y:
+                stones.pop(-1)
+                stones.pop(-1)
+                stones.append(abs(y-x))
+            else:
+                stones.pop(-1)
+                stones.pop(-1)
+            print(stones)
+            print("------")
+        if len(stones) == 2:
+            x = stones[-1]
+            y = stones[-2]
+            if x != y:
+                stones.pop(-1)
+                stones.pop(-1)
+                stones.append(abs(y - x))
+            elif x == y:
+                return 0
+        return stones[0]
+
+
+stones = [2,2]
+s = Solution()
+print(s.lastStoneWeight(stones))
