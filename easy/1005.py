125c34c99876854590b447c28bc8a33ee27f916b
Sandi_Navoiy <santes.au@gmail.com>
2024-02-13 11:04:08 +0300

    227

---
+++ easy/1005.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestSumAfterKNegations(self, nums: list[int], k: int) -> int:
+        ''''''
+        while k > 0:
+            x = nums.index(min(nums))
+            nums[x] = nums[x] * -1
+            k = k - 1
+        return sum(nums)
+
+
+
+nums = [-4,2,3]
+k = 1
+s = Solution()
+print(s.largestSumAfterKNegations(nums, k))
