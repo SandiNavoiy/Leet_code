aa1b54a0f63121aa764eb91126f1a69230283d45
Sandi_Navoiy <santes.au@gmail.com>
2024-02-21 11:54:58 +0300

    241

---
+++ easy/in_work/1854.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumPopulation(self, logs: list[list[int]]) -> int:
+        """ """
+
+logs = [[1993,1999],[2000,2010]]
+s = Solution()
+print(s.maximumPopulation(logs))
