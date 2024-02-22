be89fbd4aea04c02277981e4aba0c38c875e1261
Sandi_Navoiy <santes.au@gmail.com>
2024-02-22 19:10:41 +0300

    245

---
+++ easy/2341.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfPairs(self, nums: list[int]) -> list[int]:
+        """"""
+        inn = 0
+        inn_1 = 0
+        x = {}
+        for i in nums:
+            x[i] = nums.count(i)
+        for i in x.values():
+            if i % 2 == 0:
+                inn = inn + int(i/2)
+            else:
+                inn = inn + int(i / 2)
+                inn_1 = inn_1 +1
+
+
+        return [inn, inn_1]
+
+
+
+nums = [1, 3, 2, 1, 3, 2, 2]
+s = Solution()
+print(s.numberOfPairs(nums))
--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,4 +1,5 @@
-ops = ["5","-2","4","C","D","9","+","+"]
-for i in ops:
-    if i.isdigit():
-        print(i)
+ops = ["5","-2","4"]
+ddd ='758757689'
+ggg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x = map(lambda x: x+2, ggg)
+print(list(x))
