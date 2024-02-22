f520e9e78577e88e98238ef4188f6841b28a6d55
Sandi_Navoiy <santes.au@gmail.com>
2024-02-22 23:14:48 +0300

    246

---
+++ easy/2578.py
@@ -0,0 +1,13 @@
+class Solution:
+    def splitNum(self, num: int) -> int:
+        ''''''
+        new = list(str(num))
+        new.sort()
+        x = [new[i] for i in range(len(new)) if i % 2 == 0]
+        y = [new[i] for i in range(len(new)) if i % 2 != 0]
+        return int(''.join(x)) + int(''.join(y))
+
+
+num = 4325
+s = Solution()
+print(s.splitNum(num))
