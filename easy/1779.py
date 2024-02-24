16a35f1fe340d936cf7e7aaaf59fc4c66984a531
Sandi_Navoiy <santes.au@gmail.com>
2024-02-24 13:11:14 +0300

    252

---
+++ easy/1779.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nearestValidPoint(self, x: int, y: int, points: list[list[int]]) -> int:
+        ''''''
+        new = []
+        new1 = []
+        for i in points:
+            if x == i[0] or y == i[1]:
+                new.append(i)
+        if len(new) == 0:
+            return -1
+
+
+        sss  = abs(sum(new[0]) - (x + y))
+        new1.append(new[0])
+        for i in new:
+            if abs(sum(i) - (x + y)) < sss:
+
+                sss = abs(sum(i) - (x + y))
+                new1.clear()
+                new1.append(i)
+        if len(new1) == 0:
+            return -1
+        return points.index(new1[0])
+
+
+x = 3
+y = 4
+points = [[3,4]]
+s =  Solution()
+print(s.nearestValidPoint(x, y, points))
