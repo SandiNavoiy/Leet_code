cb704691dd2fe0686e6b69708bfd905767d3073d
Sandi_Navoiy <santes.au@gmail.com>
2024-02-24 14:44:49 +0300

    253+

--- easy/118.py
+++ easy/118.py
@@ -1,10 +1,19 @@
+import time
+
+
 class Solution:
     def generate(self, numRows: int) -> list[list[int]]:
-        """"""
+        start_time = time.time()  # Record the start time
+
         row = [1]
         for i in range(numRows):
             print(row)
             row = [sum(x) for x in zip([0] + row, row + [0])]
+
+        end_time = time.time()  # Record the end time
+        execution_time = end_time - start_time  # Calculate the execution time
+        print(f"Execution time: {execution_time} seconds")
+
         return row
 
 
