2563816d95a9ff567afba7794be86d471fa70c3f
Sandi_Navoiy <santes.au@gmail.com>
2024-02-25 23:04:24 +0300

    256

---
+++ easy/2558.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pickGifts(self, gifts: list[int], k: int) -> int:
+        ''''''
+        gifts.sort()
+        for i in range(k):
+            gifts[-1] = int(gifts[-1] ** 0.5)
+            gifts.sort()
+        return sum(gifts)
+
+gifts = [25,64,9,4,100]
+k = 4
+s = Solution()
+print(s.pickGifts(gifts, k))
