19caf65e9dd2a317f8e6b530fe4c56133f42d7fa
Sandi_Navoiy <santes.au@gmail.com>
2024-02-25 23:05:29 +0300

    256+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -3,7 +3,7 @@ ddd ='758757689'
 ggg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 x = map(lambda x: x+2, ggg)
 print(list(x))
-
+gfjhk
 # start_time = time.time()  # Record the start time
 #
 # end_time = time.time()  # Record the end time
