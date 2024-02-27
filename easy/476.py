28c1d993b00d3bf106c86c762f125cf4de560db2
Sandi_Navoiy <santes.au@gmail.com>
2024-02-27 14:18:30 +0300

    261

---
+++ easy/476.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        ''''''
+        x = bin(num)[2:]
+        y = ""
+        for i in x:
+            if i == "1":
+                y = y + "0"
+            else:
+                y = y + "1"
+
+        return int(y,2)
+num = 5
+
+s = Solution()
+print(s.findComplement(num))
