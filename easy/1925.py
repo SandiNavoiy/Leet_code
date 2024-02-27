96e761092fc7069c614481542f064ce55585c3ba
Sandi_Navoiy <santes.au@gmail.com>
2024-02-27 14:50:50 +0300

    263

---
+++ easy/1925.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+
+class Solution:
+    def countTriples(self, n: int) -> int:
+        """"""
+        x = 0
+        for i in range(1, n + 1):
+
+            for j in range(1, n + 1):
+                if sqrt(i * i + j * j) <= n and sqrt(i * i + j * j) % 1 == 0:
+                    x += 1
+
+        return x
+
+
+n = 5
+s = Solution()
+print(s.countTriples(n))
