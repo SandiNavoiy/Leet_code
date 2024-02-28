b3a168ebbef0422059d70e3b402c0a22bb384372
Sandi_Navoiy <santes.au@gmail.com>
2024-02-28 19:40:21 +0300

    271

---
+++ easy/2833.py
@@ -0,0 +1,19 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        ''''''
+        step = 0
+
+        if moves.count('L') >=moves.count('R'):
+            new = [x if x != "_" else "L" for x in moves]
+        else:
+            new = [x if x != "_" else "R" for x in moves]
+        for i in new:
+            if i == "R":
+                step += 1
+            else:
+                step -= 1
+        return abs(step)
+
+moving = "L_RL__R"
+s = Solution()
+print(s.furthestDistanceFromOrigin(moving))
