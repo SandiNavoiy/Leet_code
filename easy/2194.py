3a1dbc6c38bb230bec66410111b585e2b56726d1
Alex <santes.au@gmail.com>
2024-02-28 11:34:05 +0300

    267

---
+++ easy/2194.py
@@ -0,0 +1,15 @@
+class Solution:
+    def cellsInRange(self, s: str) -> list[str]:
+        result = []
+        for i in range(ord(s[0]), ord(s[3]) + 1):
+            for j in range(int(s[1]), int(s[4]) + 1):
+                result.append(chr(i) + str(j))
+        return result
+
+
+s = "K1:L2"
+sol = Solution()
+print(sol.cellsInRange(s))
+
+b = 80
+print(chr(b))
