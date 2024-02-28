061bc31de67821e9d38ea4535b2dc3e3f86c408f
Sandi_Navoiy <santes.au@gmail.com>
2024-02-28 18:47:18 +0300

    269

---
+++ easy/242.py
@@ -0,0 +1,15 @@
+
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ''''''
+        if Counter(s) == Counter(t):
+            return True
+        return False
+
+
+
+s = "anagram"
+t = "nagaram"
+sol = Solution()
+print(sol.isAnagram(s, t))
