824b9ca2826a780afbbcc26010d7a59acb08291d
Sandi_Navoiy <santes.au@gmail.com>
2024-02-28 19:19:53 +0300

    270

---
+++ easy/917.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        ''''''
+        new = [x for x in s if x.isalpha()]
+        new.reverse()
+
+        for i in range(len(s)):
+            if s[i].isalpha() == False:
+                new.insert(i, s[i])
+
+
+        return "".join(new)
+s = "ab-cd"
+sol = Solution()
+print(sol.reverseOnlyLetters(s))
+#Qedo1ct-eeLg=ntse-T!
