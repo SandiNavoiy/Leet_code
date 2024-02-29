916a753bedbcee48ee5294fe482df903a7a69fb0
Alex <santes.au@gmail.com>
2024-02-29 15:19:27 +0300

    274(1370)

---
+++ easy/in_work/1370.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        """"""
+        new = ""
+        www = set(s)
+        www = list(www)
+        www = sorted(www)
+        rrr = sorted(www, reverse=True)
+        www = "".join(www)
+        rrr = "".join(rrr)
+        while len(s) > len(new):
+            new = new + www
+            new = new + rrr
+
+        return new
+
+
+s = "rat"
+sol = Solution()
+print(sol.sortString(s))
