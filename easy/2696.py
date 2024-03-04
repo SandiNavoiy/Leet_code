8dd175f37bcd52c7f5306da4a010c5e2f2ad6708
Alex <santes.au@gmail.com>
2024-03-04 10:47:43 +0300

    286

---
+++ easy/2696.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        """"""
+        while "AB" in s or "CD" in s:
+            s = s.replace("AB", "")
+            s = s.replace("CD", "")
+        return len(s)
+
+
+s = "ABFCACDB"
+sol = Solution()
+print(sol.minLength(s))
