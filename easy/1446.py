cc07851d50599b6d7ffa6466a1d511169c549395
Alex <santes.au@gmail.com>
2024-03-05 10:06:53 +0300

    289

---
+++ easy/1446.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxPower(self, s):
+        cnt = 0
+        m = 0
+        for i in range(1, len(s)):
+            if s[i - 1] == s[i]:
+                cnt += 1
+                m = max(cnt, m)
+            else:
+                cnt = 0
+        return m + 1
+
+
+s = "leetcode"
+sol = Solution()
+print(sol.maxPower(s))
