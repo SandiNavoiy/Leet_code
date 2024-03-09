80990df29d1947534b788a4f209186b57bb7c225
Sandi_Navoiy <santes.au@gmail.com>
2024-03-09 19:58:18 +0300

    303

---
+++ easy/1200.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumAbsDifference(self, arr: list[int]) -> list[list[int]]:
+        """Минимальная абсолютная разница"""
+        arr.sort()
+        razn = []
+        rez = []
+        for i in range(1, len(arr)):
+            razn.append(abs(arr[i] - arr[i-1]))
+        print(razn)
+        c = min(razn)
+        for i in range(1, len(arr)):
+            if abs(arr[i]-arr[i-1]) == c:
+                rez.append([arr[i-1], arr[i]])
+
+        return rez
+
+
+arr = [3,8,-10,23,19,-4,-14,27]
+s = Solution()
+print(s.minimumAbsDifference(arr))
+# [[-14,-10],[19,23],[23,27]]
--- easy/in_work/1200.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minimumAbsDifference(self, arr: list[int]) -> list[list[int]]:
-        """Минимальная абсолютная разница"""
-
-
-arr = [4, 2, 1, 3]
-s = Solution()
-print(s.minimumAbsDifference(arr))
