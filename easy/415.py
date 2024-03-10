362c11106e255e16b7cdd374ed2ba292499ddf17
Alex <santes.au@gmail.com>
2024-03-10 08:47:53 +0300

    304

---
+++ easy/415.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        """"""
+        return str(int(num1) + int(num2))
+
+
+num1 = "11"
+num2 = "123"
+s = Solution()
+print(s.addStrings(num1, num2))
