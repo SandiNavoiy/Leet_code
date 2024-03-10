9305d834821e9d5c6a1c25ab209012ddbd1db9a3
Sandi_Navoiy <santes.au@gmail.com>
2024-03-10 16:03:13 +0300

    306

---
+++ medium/2785.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        """"""
+        x = ('a', 'e', 'i', 'o',  'u', 'A', 'E', 'I', 'O',  'U')
+        new = []
+        for i in s:
+            if i in x:
+                new.append(i)
+        new.sort()
+        ne = 0
+        new_s = list(s)
+        for i in range(len(new_s)):
+            if new_s[i] in x:
+                new_s[i] = new[ne]
+                ne += 1
+
+        return  "".join(new_s)
+
+
+
+s = "lEetcOde"
+sol = Solution()
+print(sol.sortVowels(s))
