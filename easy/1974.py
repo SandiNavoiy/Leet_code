cf2864aadec989e491226da80da32d6d7f9ca21f
Sandi_Navoiy <santes.au@gmail.com>
2024-03-12 09:27:12 +0300

    312

---
+++ easy/1974.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minTimeToType(self, word: str) -> int:
+        time = len(word)
+        cur = 'a'
+        for w in word:
+            diff = abs(ord(w) - ord(cur))
+            time += min(diff, 26 - diff)
+            cur = w
+        return time
--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,2 +1,1 @@
-x = [9, 1, 7, 8, 9, 2, 3, 4, 6]
-print(x.count(9))
+print(ord('e') - ord('a'))
