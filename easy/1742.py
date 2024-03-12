e149068ea679bf5c92cb8044bf80d33b188ae6a6
Sandi_Navoiy <santes.au@gmail.com>
2024-03-12 10:02:44 +0300

    314

---
+++ easy/1742.py
@@ -0,0 +1,16 @@
+import collections
+
+
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        ballBox = collections.defaultdict(int)
+
+        for i in range(lowLimit, highLimit + 1):
+            ballSum = sum([int(i) for i in str(i)])
+            ballBox[ballSum] += 1
+
+        return max(ballBox.values())
+lowLimit = 1
+highLimit = 10
+s = Solution()
+print(s.countBalls(lowLimit, highLimit))
