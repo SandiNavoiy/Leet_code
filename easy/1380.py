37739ecf009246f9a049e5bb2b48f1d2cec1150d
Sandi_Navoiy <santes.au@gmail.com>
2024-03-12 09:41:55 +0300

    312

--- easy/1380.py
+++ easy/1380.py
@@ -1,14 +1,23 @@
 class Solution:
-    def luckyNumbers(self, matrix: list[list[int]]) -> list[int]:
-        """"""
-        ss = 0
-        for i in range(len(matrix)):
-            matrix[i].sort()
-            if matrix[i][0] > ss:
-                ss = matrix[i][0]
-        return ss
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        ''''''
+        slov = {"a": "0", "b": "1", "c": "2", "d": "3", "e": "4", "f": "5", "g": "6", "h": "7", "i": "8", "j": "9"}
+        word1, word2, word3 = "", "", ""
+
+        for i in firstWord:
+            word1 = word1 + slov[i]
+
+        for i in secondWord:
+            word2 = word2 + slov[i]
+
+        for i in targetWord:
+            word3 = word3 + slov[i]
+        print(word1, word2, word3)
+        return int(word1) + int(word2) == int(word3)
 
 
-matrix = [[-5, 0], [0, 0]]
+firstWord = "acb"
+SecondWord = "cba"
+targetWord = "cdb"
 s = Solution()
-print(s.luckyNumbers(matrix))
+print(s.isSumEqual(firstWord, SecondWord, targetWord))
