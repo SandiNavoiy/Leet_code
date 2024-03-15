b0d593e2027f03fba0e5ff8cfaae8c61f1f84764
Sandi_Navoiy <santes.au@gmail.com>
2024-03-15 17:04:12 +0300

    324

---
+++ easy/461.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        ''''''
+        return bin(x ^ y).count("1")
+x = 1
+y = 4
+s = Solution()
+print(s.hammingDistance(x,y))
