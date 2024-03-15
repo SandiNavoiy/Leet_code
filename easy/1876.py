c48fbf362c94135bc013ab213e4dc8846be2ae17
Sandi_Navoiy <santes.au@gmail.com>
2024-03-15 16:51:54 +0300

    323

---
+++ easy/1876.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        ''''''
+        new = []
+        count  = 0
+        for i in range(len(s)-2):
+            sss = s[i:i + 3]
+
+            if len(sss) == len(set(sss)):
+                count  += 1
+
+        return count
+
+
+
+s = "aababcabc"
+sol = Solution()
+print(sol.countGoodSubstrings(s))
