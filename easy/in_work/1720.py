633081e6b8bfbeae8f27a4499cd9794beea6dc56
Sandi_Navoiy <santes.au@gmail.com>
2024-03-24 23:06:39 +0300

    353+

---
+++ easy/in_work/1720.py
@@ -0,0 +1,10 @@
+class Solution:
+    def decode(self, encoded: list[int], first: int) -> list[int]:
+        ''''''
+        new = [first]
+
+        return new
+encoded = [1,2,3]
+first = 1
+s = Solution()
+print(s.decode(encoded, first))
