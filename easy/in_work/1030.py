dcb6df4460fee1ffdc2f92aae60ca905d93c5061
Sandi_Navoiy <santes.au@gmail.com>
2024-03-26 15:25:39 +0300

    357

---
+++ easy/in_work/1030.py
@@ -0,0 +1,14 @@
+class Solution:
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> list[list[int]]:
+        ''''''
+        new = [[x,y] for x in range(rows)  for y in range(cols)]
+        new.sort(key=lambda point: abs(point[0] - rCenter) + abs(point[1] - cCenter))
+        return new
+
+
+rows = 1
+cols = 2
+rCenter = 0
+cCenter = 0
+s = Solution()
+print(s.allCellsDistOrder(rows, cols, rCenter, cCenter))
