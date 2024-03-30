64e7268cab53835c7f5531c09f3770661da7647a
Sandi_Navoiy <santes.au@gmail.com>
2024-03-30 21:15:44 +0300

    374

---
+++ easy/2395.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findSubarrays(self, nums: list[int]) -> bool:
+        ''''''
+        d= {}
+        for i in range(len(nums)-1):
+            ss = nums[i] + nums[i+1]
+            if ss not in d.keys():
+                d[ss] = [nums[i], nums[i+1]]
+            else:
+                return True
+
+
+        return False
+nums = [1,2,3,4,5]
+s = Solution()
+print(s.findSubarrays(nums))
