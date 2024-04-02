e307d93130c4f4441418b849c5dff7c298936c1e
Alex <santes.au@gmail.com>
2024-04-02 09:24:23 +0300

    383

--- easy/762.py
+++ easy/762.py
@@ -4,7 +4,6 @@ class Solution:
         ss = (2, 3, 5, 7, 11, 13, 17, 19)
         count = 0
         for i in range(left, right + 1):
-
             if bin(i)[2:].count("1") in ss:
                 count = count + 1
 
