6c4ea7bb155f6bef2c6185735125076797bd5272
Sandi_Navoiy <santes.au@gmail.com>
2024-04-03 12:10:57 +0300

    386

---
+++ easy/1455.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        ''''''
+        new = sentence.split()
+        for i in range(len(new)):
+            if searchWord in new[i][:len(searchWord)]:
+                return i + 1
+
+        return -1
+sentence = "hellohello hellohellohello"
+searchWord = "ell"
+s = Solution()
+print(s.isPrefixOfWord(sentence,searchWord ))
