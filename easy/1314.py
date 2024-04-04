4dd9a15807417117c0ec228b90192ebe8b027832
Sandi_Navoiy <santes.au@gmail.com>
2024-04-04 21:54:46 +0300

    394+

---
+++ easy/1314.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        ''''''
+        x = dict(Counter(word))
+        print(sum(x.values()))
+
+        return x
+word = "xycdefghij"
+s = Solution()
+print(s.minimumPushes(word))
