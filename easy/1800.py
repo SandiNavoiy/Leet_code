f71bafb3dffae5a153530d1ed1d47c99b67bc270
Alex <santes.au@gmail.com>
2024-04-04 10:15:06 +0300

    392

---
+++ easy/1800.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxAscendingSum(self, nums: list[int]) -> int:
+        res = 0
+
+        curr = nums[0]
+        for i, n in enumerate(nums[1:]):
+
+            if n <= nums[i]:
+                res = max(res, curr)
+                curr = n
+            else:
+                curr += n
+
+        return max(res, curr)
+
+
+nums = [10, 20, 30, 5, 10, 50]
+s = Solution()
+print(s.maxAscendingSum(nums))
