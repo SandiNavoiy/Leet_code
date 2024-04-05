851f3aedb2d744f2752b39dee68765a50b17cc8e
Sandi_Navoiy <santes.au@gmail.com>
2024-04-05 18:35:17 +0300

    396

---
+++ easy/1422.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ''''''
+        summ = 0
+
+        for i in range(1,len(s)):
+            if (s[0:i].count("0") + s[i:].count("1")) > summ:
+                summ = (s[0:i].count("0") + s[i:].count("1"))
+
+
+
+
+        return summ
+
+
+s = "011101"
+sol = Solution()
+print(sol.maxScore(s))
