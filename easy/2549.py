2880e8f8415e17b205e400bc8cabe486a0d9d1f7
Sandi_Navoiy <santes.au@gmail.com>
2024-04-06 10:57:42 +0300

    402

---
+++ easy/2549.py
@@ -0,0 +1,10 @@
+class Solution:
+    def distinctIntegers(self, n: int) -> int:
+        ''''''
+        if n> 1:
+            return n -1
+        else:
+            return 1
+n = 5
+s = Solution()
+print(s.distinctIntegers(n))
