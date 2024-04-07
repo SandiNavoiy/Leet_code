fd2e9d62af851f077d79c54d0d46ce132a15385a
Sandi_Navoiy <santes.au@gmail.com>
2024-04-07 10:46:34 +0300

    404

---
+++ easy/796.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        ''''''
+        if len(s) != len(goal):
+            return False
+        s= s + s
+        if goal in s:
+            return True
+
+        return False
+s = "abcde"
+goal = "cdeab"
+sol =Solution()
+print(sol.rotateString(s, goal))
