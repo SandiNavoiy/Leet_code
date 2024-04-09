9dd21043055bae73407154096df322e6849b798b
Sandi_Navoiy <santes.au@gmail.com>
2024-04-09 22:37:26 +0300

    416

---
+++ easy/in_work/2379.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        ''''''
+        x = blocks.count('W')
+
+        for i in range(len(blocks)-k+1):
+            print(blocks[i:i+k])
+            if blocks[i:i+k].count('W') < x:
+                x = blocks[i:i+k].count('W')
+        return x
+blocks = "WWBBBWBBBBBWWBWWWB"
+k = 16
+s = Solution()
+print(s.minimumRecolors(blocks, k))
