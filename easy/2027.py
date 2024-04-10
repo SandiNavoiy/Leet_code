63c400e64273fa63aaf63caa99980296868f2f7e
Alex <santes.au@gmail.com>
2024-04-10 12:25:03 +0300

    417

---
+++ easy/2027.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        ans = i = 0
+        while i < len(s):
+            if s[i] == "O":
+                i += 1
+            else:
+                i += 3
+                ans += 1
+        return ans
+
+
+s = "OXOX"
+sol = Solution()
+print(sol.minimumMoves(s))
