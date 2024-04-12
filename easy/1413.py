d8a84611cb4c7f81893ec1b4e9a7aafce84c4c42
Sandi_Navoiy <santes.au@gmail.com>
2024-04-12 14:05:56 +0300

    429

---
+++ easy/1413.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minStartValue(self, nums: list[int]) -> int:
+        for i in range(1,len(nums)):
+            nums[i]+=nums[i-1]
+
+        temp=min(nums)
+        if temp>0:
+            return 1
+        else:
+            return -temp+1
+
+nums = [-3,2,-3,4,2]
+s = Solution()
+print(s.minStartValue(nums))
