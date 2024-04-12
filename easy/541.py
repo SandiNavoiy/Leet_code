2363e14d17a2df8836963727e344034d08a40b68
Sandi_Navoiy <santes.au@gmail.com>
2024-04-12 15:45:34 +0300

    434

---
+++ easy/541.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        i = 0
+        while i < len(s):
+            s = s[:i] + s[i:i + k][::-1] + s[i + k:]
+            i = i + 2 * k
+        return s
+
+
+s = "abcdefg"
+k = 1
+sol = Solution()
+print(sol.reverseStr(s, k))
+#"cbadef"
