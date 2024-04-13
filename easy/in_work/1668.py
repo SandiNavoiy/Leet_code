a77855cc8dbfdb36c4f2a1d084e7bb14e67df266
Sandi_Navoiy <santes.au@gmail.com>
2024-04-13 22:52:38 +0300

    449

---
+++ easy/in_work/1668.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        if len(sequence)<len(word):
+            return 0
+        k=1
+        ans=0
+
+        while k*word in sequence:
+            ans+=1
+            k+=1
+        return ans
+
+sequence = "aaaba aaaba aab a aaaba aaaba aaaba aaaba"
+word = "aaaba"
+s = Solution()
+print(s.maxRepeating(sequence, word))
