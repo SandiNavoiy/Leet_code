3ca4a4a8c6319c927c76b93df2f4c1c707314288
Sandi_Navoiy <santes.au@gmail.com>
2024-04-13 13:55:53 +0300

    440

---
+++ medium/215.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        ''''''
+        nums.sort(reverse=True)
+        return nums[k-1]
+nums = [3,2,1,5,6,4]
+k = 2
+s = Solution()
+print(s.findKthLargest(nums, k))
