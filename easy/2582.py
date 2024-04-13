2ab2c573fe8dcf869193cfefc896fc92cae89c18
Sandi_Navoiy <santes.au@gmail.com>
2024-04-13 15:51:00 +0300

    444

---
+++ easy/2582.py
@@ -0,0 +1,20 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        ''''''
+        if time < n:
+            return time +1
+        elif (time // (n-1)) % 2 == 0:
+            print(1)
+
+
+            return time % (n-1) + 1
+        else:
+            print(2)
+            return n- time % (n-1)
+
+n = 18
+time = 38
+
+
+s = Solution()
+print(s.passThePillow(n, time))
