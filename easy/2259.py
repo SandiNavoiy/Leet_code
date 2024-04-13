4b9928b6569277432539297f184ce575036ed42c
Sandi_Navoiy <santes.au@gmail.com>
2024-04-13 12:37:23 +0300

    437

---
+++ easy/2259.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDigit(self, number: str, digit: str) -> str:
+        ''''''
+        new  = str(number)
+        sss = 0
+        for i in range(len(new)):
+            if new[i] == str(digit):
+                if int(new[0:i] + new[i+1:]) > sss:
+                    sss = int(new[0:i] + new[i+1:])
+        return str(sss)
+number = "123"
+digit = "3"
+s = Solution()
+print(s.removeDigit(number, digit))
