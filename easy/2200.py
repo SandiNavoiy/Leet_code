898c310351c26659cce48f310d9f48d3c3ca1f72
Sandi_Navoiy <santes.au@gmail.com>
2024-04-14 20:22:30 +0300

    462

---
+++ easy/2200.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findKDistantIndices(self, nums: list[int], key: int, k: int) -> list[int]:
+        ''''''
+        new = []
+        x = len(nums)
+        for i in range(x):
+            for j in range(max(0, i - k), min(len(nums), i + k + 1)):
+                if nums[j] == key:
+                    if i not in new:
+                        new.append(i)
+        return new
+
+nums = [2,2,2,2,2]
+key = 2
+k = 2
+s = Solution()
+print(s.findKDistantIndices(nums, key, k))
