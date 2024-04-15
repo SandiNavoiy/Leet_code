9343e467a87787a8a198345d8d1d386332f3a432
Alex <santes.au@gmail.com>
2024-04-15 09:53:41 +0300

    464

--- easy/in_work/836.py
+++ easy/in_work/836.py
@@ -1,6 +1,11 @@
 class Solution:
     def isRectangleOverlap(self, rec1: list[int], rec2: list[int]) -> bool:
-        return rec1[2] > rec2[0] and rec1[3] > rec2[1] and rec2[2] > rec1[0] and rec2[3] > rec1[1]
+        return (
+            rec1[2] > rec2[0]
+            and rec1[3] > rec2[1]
+            and rec2[2] > rec1[0]
+            and rec2[3] > rec1[1]
+        )
 
 
 rec1 = [0, 0, 2, 2]
