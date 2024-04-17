98a5102d4d6523382635b225f3efd77e037ca127
Alex <santes.au@gmail.com>
2024-04-17 10:05:33 +0300

    475

--- easy/598.py
+++ easy/598.py
@@ -7,12 +7,6 @@ class Solution:
             min_row = min(min_row, op[0])  # Обновляем минимальное количество строк
             min_col = min(min_col, op[1])  # Обновляем минимальное количество столбцов
 
-        return min_row * min_col  # Возвращаем произведение минимальных количеств строк и столбцов
-
-
-
-m = 3
-n = 3
-ops = [[2, 2], [3, 3]]
-s = Solution()
-print(s.maxCount(m, n, ops))
+        return (
+            min_row * min_col
+        )  # Возвращаем произведение минимальных количеств строк и столбцов
