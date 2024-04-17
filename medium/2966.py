358a2f4a61a3e6c440ad6bb4c23ed737216b1e7a
Sandi_Navoiy <santes.au@gmail.com>
2024-04-17 20:25:15 +0300

    478

---
+++ medium/2966.py
@@ -0,0 +1,19 @@
+class Solution:
+    def divideArray(self, nums: list[int], k: int) -> list[list[int]]:
+        ''''''
+        new = []
+        nums.sort()
+        print(nums)
+
+        for i in range(0, len(nums),3):
+
+            if nums[i+2] - nums[i] <= k:
+                new.append(nums[i:i+3])
+            else:
+                return []
+        return new
+
+nums = [1,3,4,8,7,9,3,5,1]
+k = 2
+s = Solution()
+print(s.divideArray(nums,k))
