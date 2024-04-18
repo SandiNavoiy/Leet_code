368fd7116fe5d8901f98114d108807ffeba6132d
Sandi_Navoiy <santes.au@gmail.com>
2024-04-18 14:34:20 +0300

    481

---
+++ medium/2221.py
@@ -0,0 +1,15 @@
+class Solution:
+    def triangularSum(self, nums: list[int]) -> int:
+        ''''''
+        if len(nums) == 1:
+            return nums[0]
+
+        while len(nums) > 1:
+            new = []
+            for i in range(len(nums)-1):
+                new.append((nums[i] + nums[i+1]) %10)
+            nums = new
+        return nums[0]
+nums = [5]
+s = Solution()
+print(s.triangularSum(nums))
