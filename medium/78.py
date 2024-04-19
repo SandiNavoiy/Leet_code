31f00554841f18ad15ea3ae3166b33d304dda1e4
Sandi_Navoiy <santes.au@gmail.com>
2024-04-19 19:43:43 +0300

    483

---
+++ medium/78.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        ''''''
+        new = []
+        for i in range(len(nums)+1):
+            x = list(combinations(nums,i))
+            for j in range(len(x)):
+                new.append(list(x[j]))
+
+
+
+
+        return new
+
+
+s = Solution()
+nums = [1,2,3]
+print(s.subsets(nums))
