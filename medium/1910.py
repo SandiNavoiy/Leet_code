76391a9cfca84d8af1e04a40238c4a68962630fe
Sandi_Navoiy <santes.au@gmail.com>
2024-04-20 15:28:02 +0300

    485

---
+++ medium/1910.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        ''''''
+        while part in s:
+            i = s.find(part)
+            s = s[:i] + s[i + len(part):]
+        return s
+
+s = "aabababa"
+part = "aba"
+sol = Solution()
+print(sol.removeOccurrences(s,part))
