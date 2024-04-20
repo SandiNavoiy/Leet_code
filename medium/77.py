4adc68647019aa8615de5536c1d9c21626a352f5
Sandi_Navoiy <santes.au@gmail.com>
2024-04-20 20:38:29 +0300

    488

---
+++ medium/77.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+class Solution:
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        ''''''
+        temp = [i for i in range(1,n+1 )]
+        x = list(combinations(temp,k))
+
+        return x
+n = 4
+k = 2
+s = Solution()
+print(s.combine(n, k))
