da06c4f5ec820301f84375e44d1ea4b35c856e9d
Alex <santes.au@gmail.com>
2024-04-25 12:04:53 +0300

    507+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,5 +1,20 @@
-from random import choice
-from string import ascii_letters
+from itertools import combinations
 
 
-print(''.join(choice(ascii_letters) for i in range(12)))
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        """"""
+        inn = []
+        seen = set()
+        for i in combinations(nums, 3):
+            comb_sorted = tuple(sorted(i))
+            if sum(comb_sorted) == 0 and comb_sorted not in seen:
+                inn.append(comb_sorted)
+                seen.add(comb_sorted)
+
+        return inn
+
+
+nums = [-1, 0, 1, 2, -1, -4]
+s = Solution()
+print(s.threeSum(nums))
--- medium/in_work/34.py
+++ medium/in_work/34.py
@@ -1,40 +1,38 @@
 class Solution:
     def searchRange(self, nums: list[int], target: int) -> list[int]:
-        ''''''
-        high = len(nums)-1
+        """"""
+        high = len(nums) - 1
         low = 0
-        if len(nums)  == 1:
+        if len(nums) == 1:
             if target in nums:
                 return [0, 0]
             else:
                 return [-1, -1]
-
-
 
         while low <= high:
             medium = (high + low) // 2
             print(medium)
 
             if nums[medium] == target:
-                if target == nums[medium-1]:
-                    if medium-1 < 0:
-                        return [medium, medium+1]
+                if target == nums[medium - 1]:
+                    if medium - 1 < 0:
+                        return [medium, medium + 1]
 
-                    return [medium-1, medium]
-                elif target == nums[medium+1]:
-                    return [medium, medium+1]
+                    return [medium - 1, medium]
+                elif medium + 1 <= len(nums) - 1 and target == nums[medium + 1]:
+                    return [medium, medium + 1]
                 else:
                     return [medium, medium]
             elif nums[medium] > target:
-
-                high = medium -1
+                high = medium - 1
 
             elif nums[medium] < target:
-
-                low = medium +1
+                low = medium + 1
 
         return [-1, -1]
-nums = [1,4]
+
+
+nums = [1, 4]
 target = 4
 s = Solution()
 print(s.searchRange(nums, target))
