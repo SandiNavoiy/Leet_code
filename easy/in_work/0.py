c97e3d3731b678c21c0ec269ef5a03fbc3a496c4
Sandi_Navoiy <santes.au@gmail.com>
2024-04-25 19:19:30 +0300

    508+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -4,13 +4,8 @@ from itertools import combinations
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         """"""
-        seen = set()
-        for i in combinations(tuple(nums), 3):
+        seen = set(tuple(sorted(i)) for i in combinations(tuple(nums), 3) if sum(i) == 0)
 
-            if sum(i) == 0:
-                comb_sorted = tuple(sorted(i))
-
-                seen.add(comb_sorted)
         return list(seen)
 
 
--- medium/15.py
+++ medium/15.py
@@ -1,16 +1,10 @@
 from itertools import combinations
 
-
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         """"""
-        seen = set()
-        for i in combinations(tuple(nums), 3):
+        seen = set(tuple(sorted(i)) for i in combinations(tuple(nums), 3) if sum(i) == 0)
 
-            if sum(i) == 0:
-                comb_sorted = tuple(sorted(i))
-
-                seen.add(comb_sorted)
         return list(seen)
 
 
