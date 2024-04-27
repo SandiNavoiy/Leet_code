41aa9e19c736c23b41f3c6822dbe56ae1200252c
Alex <santes.au@gmail.com>
2024-04-27 11:36:29 +0300

    515+

---
+++ easy/in_work/2224.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertTime(self, current: str, correct: str) -> int:
+        """"""
+
+
+current = "02:30"
+correct = "04:35"
+s = Solution()
+print(s.convertTime(current, correct))
