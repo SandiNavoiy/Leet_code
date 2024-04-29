e16acab5b9d6c9cd4150dde3930c266eafaa97e4
Sandi_Navoiy <santes.au@gmail.com>
2024-04-29 12:40:39 +0300

    520

--- easy/3120.py
+++ easy/3120.py
@@ -1,10 +1,15 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         """"""
-        v = 0
+
+
+        v = set()
         for i in word:
-            if ord(i) < 65:
-                if
+
+            if ord(i) >= 97 and i.upper() in word:
+                v.add(i)
+        return len(v)
+
 
 
 word = "aaAbcBC"
