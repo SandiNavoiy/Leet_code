fc299a8366962d042dd7c2b4044fce431b42998d
Alex <santes.au@gmail.com>
2024-04-29 10:29:55 +0300

    519+

---
+++ easy/in_work/3120.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        """"""
+        v = 0
+        for i in word:
+            if ord(i) < 65:
+                if
+
+
+word = "aaAbcBC"
+s = Solution()
+print(s.numberOfSpecialChars(word))
