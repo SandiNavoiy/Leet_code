98e3aa79d59d6e5de113010d6d740ee9d55903cd
Sandi_Navoiy <santes.au@gmail.com>
2024-04-29 13:56:38 +0300

    522

---
+++ easy/3127.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canMakeSquare(self, grid: list[list[str]]) -> bool:
+        ''''''
+        grid1 = [
+            [grid[0][0], grid[0][1], grid[1][0], grid[1][1]],
+            [grid[0][1], grid[0][2], grid[1][1], grid[1][2]],
+            [grid[1][0], grid[1][1], grid[2][0], grid[2][1]],
+            [grid[1][1], grid[1][2], grid[2][1], grid[2][2]],
+        ]
+        for i in grid1:
+            if i.count("B") != 2:
+                return True
+        return False
+
+s = Solution()
+grid = [["B","W","B"],["B","W","W"],["B","W","B"]]
+print(s.canMakeSquare(grid))
