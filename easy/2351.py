c49fc74cf568913201618a00945f262bc95ee500
Sandi_Navoiy <santes.au@gmail.com>
2024-05-01 20:23:14 +0300

    525

---
+++ easy/2351.py
@@ -0,0 +1,15 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        """"""
+        new = []
+        for i in s:
+            if i not in new:
+                new.append(i)
+            else:
+                return i
+
+
+
+s = "abccbaacz"
+sol = Solution()
+print(sol.repeatedCharacter(s))
