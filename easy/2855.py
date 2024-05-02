554334f956bf44a0b320f012f0a3eef00e7c203a
Alex <santes.au@gmail.com>
2024-05-02 10:36:39 +0300

    528

--- easy/2855.py
+++ easy/2855.py
@@ -1,11 +1,11 @@
 class Solution:
     def minimumRightShifts(self, nums: List[int]) -> int:
-        i=nums.index(max(nums))
-        nums=nums[i+1:]+nums[:i+1]
-        for j in range(len(nums)-1):
-            if nums[j+1]<nums[j]:
+        i = nums.index(max(nums))
+        nums = nums[i + 1 :] + nums[: i + 1]
+        for j in range(len(nums) - 1):
+            if nums[j + 1] < nums[j]:
                 return -1
-        return len(nums)-i-1
+        return len(nums) - i - 1
 
 
 nums = [3, 4, 5, 1, 2]
