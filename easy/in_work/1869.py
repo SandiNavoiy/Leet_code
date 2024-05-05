00976b8c6a3d1a104d4cc992db2a5a7dee12f6d1
Sandi_Navoiy <santes.au@gmail.com>
2024-05-05 21:03:08 +0300

    539+

---
+++ easy/in_work/1869.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkZeroOnes(self, s: str) -> bool:
+        ''''''
+        col_1 = 0
+        col_0 = 0
+        temp_0 = 0
+        temp_1 = 0
+
+        for i in s:
+
+            if i == "0":
+                if temp_1> col_1:
+                    col_1 =  temp_1
+                temp_1 = 0
+                temp_0 = temp_0 + 1
+            else:
+                if temp_0 > col_0:
+                    col_0 = temp_0
+                temp_0 = 0
+                temp_1 = temp_1 + 1
+        print(col_0)
+        print(col_1)
+
+        if col_0 < col_1:
+            return True
+        return False
+s = "111000"
+sol = Solution()
+print(sol.checkZeroOnes(s))
