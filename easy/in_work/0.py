e35cac8521d802d671dc5bfa5ff9ea0662bb3964
Alex <santes.au@gmail.com>
2024-05-07 16:58:02 +0300

    550+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,16 +1,55 @@
-def linear_search(arr, target):
-    n = len(arr)
-    rez = []
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
 
-    for i in range(n):
-        if arr[i] == target:
-            rez.append(i)
-
-    if len(rez) == 0:
-        return -1
-    return rez
+    def __str__(self):
+        return str(self.value)
 
 
-idx = linear_search([1, 3, 3, 4], 3)
-print(idx)
-[1, 2]
+class List:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        current = self.head
+        while current.next_node is not None:
+            current = current.next_node
+
+        current.next_node = Node(value)
+
+    def prepend(self, value):
+        # Добавьте ваш код тут.
+
+        new_node = Node(value)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def __str__(self):
+        current = self.head
+        values = "["
+
+        while current is not None:
+            end = ", " if current.next_node else ""
+            values += str(current) + end
+            current = current.next_node
+
+        return values + "]"
+
+
+lst = List()
+
+# Добавляем элементы в конец
+lst.append("A")
+lst.append("B")
+
+# Добавляем элементы в начало
+lst.prepend("C")
+lst.prepend("D")
+
+# Выводим связный список
+print(lst)
