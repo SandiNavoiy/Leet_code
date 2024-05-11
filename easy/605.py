718c1cd5f3221e76d10ce10408ddc0515f1e43a3
Alex <santes.au@gmail.com>
2024-05-11 14:11:35 +0300

    559

---
+++ easy/605.py
@@ -0,0 +1,53 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        """"""
+
+        if len(flowerbed) == 0:
+            return False
+        elif len(flowerbed) == 1 and flowerbed[0] == 0:
+            return True
+        elif len(flowerbed) == 1 and flowerbed[0] == 1 and n == 0:
+            return True
+        elif len(flowerbed) == 2 and sum(flowerbed) == 0 and n <= 1:
+            return True
+        elif len(flowerbed) == 2 and sum(flowerbed) == 0 and n > 1:
+            return False
+        elif len(flowerbed) == 2 and sum(flowerbed) != 0 and n == 0:
+            return True
+        elif len(flowerbed) == 2 and sum(flowerbed) != 0:
+            return False
+        elif len(flowerbed) == 3 and sum(flowerbed) == 0 and n == 2:
+            return True
+        if int(len(flowerbed) / 2) < n and len(flowerbed) % 2 == 0:
+            return False
+        elif len(flowerbed) % 2 != 0 and int(len(flowerbed) + 1 / 2) < n:
+            return False
+        x = 0
+        i = 1
+        end = len(flowerbed)
+        if flowerbed[0] == 0 and flowerbed[1] == 0:
+            i = 2
+            x = x + 1
+        if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+            end = end - 1
+            x = x + 1
+
+        while i <= end - 1:
+            if (
+                flowerbed[i] == 0
+                and flowerbed[max(i - 1, 0)] == 0
+                and flowerbed[min(i + 1, end)] == 0
+            ):
+                x = x + 1
+                i = i + 2
+            else:
+                i = i + 1
+        print(x)
+
+        return x >= n
+
+
+flowerbed = [0]
+n = 1
+s = Solution()
+print(s.canPlaceFlowers(flowerbed, n))
