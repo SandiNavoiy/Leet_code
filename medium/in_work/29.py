1ad21b030264c760e4685e966e09722c6144b922
Alex <santes.au@gmail.com>
2024-05-13 14:00:59 +0300

    567+

---
+++ medium/in_work/29.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        """"""
+
+
+dividend = 10
+divisor = 3
+s = Solution()
+print(s.divide(dividend, divisor))
---
+++ medium/in_work/3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """"""
+        rez = 0
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                if len(s[i:j]) == len(set(s[i:j])):
+                    rez = max(rez, len(s[i:j]))
+        return rez
+
+
+s = "abcabcbb"
+sol = Solution()
+print(sol.lengthOfLongestSubstring(s))
