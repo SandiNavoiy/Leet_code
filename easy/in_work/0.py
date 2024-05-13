0e3b62297c09174e4597005baf29b422c85ee6bf
Sandi_Navoiy <santes.au@gmail.com>
2024-05-13 18:39:05 +0300

    568+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -2,13 +2,16 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """"""
         rez = 0
-        for i in range(len(s)):
-            for j in range(i + 1, len(s) + 1):
-                if len(s[i:j]) == len(set(s[i:j])):
-                    rez = max(rez, len(s[i:j]))
-                else:
-                    break
-        return rez
-s = "abcabcbb"
+        char_dict  = {}
+        index = 0
+        for i, char in enumerate(s):
+            if char in char_dict and char_dict[char] >= index:
+                rez = max(rez, i - index)
+                index = char_dict[char] + 1
+            char_dict[char]  = i
+
+        return max(rez, len(s) - index)
+
+s = "pwwkew"
 sol = Solution()
 print(sol.lengthOfLongestSubstring(s))
