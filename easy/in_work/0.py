606e93dbbe960511dde4296765e92e99958f7a7a
Sandi_Navoiy <santes.au@gmail.com>
2024-05-13 22:35:13 +0300

    568+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,17 +1,2 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        """"""
-        rez = 0
-        char_dict  = {}
-        index = 0
-        for i, char in enumerate(s):
-            if char in char_dict and char_dict[char] >= index:
-                rez = max(rez, i - index)
-                index = char_dict[char] + 1
-            char_dict[char]  = i
 
-        return max(rez, len(s) - index)
-
-s = "pwwkew"
-sol = Solution()
-print(sol.lengthOfLongestSubstring(s))
+print(len(range(0, 10, 2)))
--- medium/in_work/29.py
+++ medium/in_work/29.py
@@ -1,9 +1,32 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         """"""
+        if dividend == divisor:
+            return 1
+        if dividend > 0 and divisor > 0:
+            flag  = 1
+        elif dividend > 0 and divisor < 0:
+            flag = 0
+        elif dividend < 0 and divisor > 0:
+            flag = 0
+        elif dividend < 0 and divisor < 0:
+            flag = 1
+        elif dividend == 0 :
+            flag = 1
+
+        dividend=  abs(dividend)
+        divisor = abs(divisor)
+        rez = 0
+        while dividend > divisor:
+            rez = rez + 1
+            dividend = dividend - divisor
+        if flag == 1:
+            return rez
+        else:
+            return rez * -1
 
 
-dividend = 10
-divisor = 3
+dividend = 1
+divisor = 1
 s = Solution()
 print(s.divide(dividend, divisor))
