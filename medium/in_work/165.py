2af3fedda2cd655d3b5c12a21ef519fa6c248d29
Alex <santes.au@gmail.com>
2024-05-18 09:36:02 +0300

    583

--- medium/in_work/165.py
+++ medium/in_work/165.py
@@ -1,13 +1,21 @@
+from itertools import zip_longest
+
+
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         """"""
-        s1 = version1.split(".")
-        s2 = version2.split(".")
+        s1 = map(int, version1.split("."))
+        s2 = map(int, version2.split("."))
+        for i, j in zip_longest(s1, s2, fillvalue=0):
+            if i > j:
+                return 1
+            elif i < j:
+                return -1
 
-        return s2
+        return 0
 
 
 s = Solution()
-version1 = "1.2"
-version2 = "1.010"
+version1 = "1.0"
+version2 = "1.0.0.0"
 print(s.compareVersion(version1, version2))
