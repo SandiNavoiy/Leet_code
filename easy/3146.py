40e1df3c1243ca11aec85a6d31fabb51429278cf
Sandi_Navoiy <santes.au@gmail.com>
2024-05-23 21:41:11 +0300

    604

---
+++ easy/3146.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPermutationDifference(self, s: str, t: str) -> int:
+        ''''''
+        rez = 0
+        for i in range(len(s)):
+            rez = rez + abs(i - t.find(s[i]))
+        return rez
+s = "abc"
+t = "bac"
+sol = Solution()
+print(sol.findPermutationDifference(s, t))
