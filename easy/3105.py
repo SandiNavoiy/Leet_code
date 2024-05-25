216fab95f6b3014071dcdb04cc507da01edc1061
Alex <santes.au@gmail.com>
2024-05-25 09:13:38 +0300

    606

---
+++ easy/3105.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: list[int]) -> int:
+        longinc = 1
+        longdec = 1
+        i = 0
+        j = 0
+        for i in range(len(nums) - 1):
+            if i < len(nums) - 1 and nums[i] < nums[i + 1]:
+                j = i
+                while j < len(nums) - 1 and nums[j] < nums[j + 1]:
+                    j += 1
+                longdec = max(j - i + 1, longdec)
+                i = j
+            i += 1
+        i = 0
+        j = 0
+        for i in range(len(nums) - 1):
+            if i < len(nums) - 1 and nums[i] > nums[i + 1]:
+                j = i
+                while j < len(nums) - 1 and nums[j] > nums[j + 1]:
+                    j += 1
+                longinc = max(j - i + 1, longinc)
+                i = j
+            i += 1
+        return max(longdec, longinc)
+
+
+nums = [1, 4, 3, 3, 2]
+s = Solution()
+print(s.longestMonotonicSubarray(nums))
