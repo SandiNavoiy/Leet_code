b264e997795320e2719f5fbe066ad76930996d7f
Sandi_Navoiy <santes.au@gmail.com>
2024-05-26 15:37:10 +0300

    609

---
+++ easy/3158.py
@@ -0,0 +1,19 @@
+from collections import Counter
+from functools import reduce
+
+
+class Solution:
+    def duplicateNumbersXOR(self, nums: list[int]) -> int:
+        """"""
+        new = [i for i in nums if nums.count(i) == 2]
+        new = list(set(new))
+
+
+
+        return reduce(lambda x, y: x^y, new, 0)
+
+
+
+nums = [1, 2, 1, 3]
+s = Solution()
+print(s.duplicateNumbersXOR(nums))
--- easy/in_work/3158.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def duplicateNumbersXOR(self, nums: list[int]) -> int:
-        """"""
-
-
-nums = [1, 2, 1, 3]
