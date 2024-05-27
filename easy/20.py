785ece9809f9c9861fa337c0f58876a38c448bfa
Sandi_Navoiy <santes.au@gmail.com>
2024-05-27 12:17:21 +0300

    613

---
+++ easy/20.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        ''''''
+        stack = list(s)
+        new = []
+        for i in  stack:
+            if i =="(" or i == "{" or i == "[":
+                new.append(i)
+            elif i == ")" and new and new[-1] == "(":
+                new.pop()
+
+            elif i == "]" and new and new[-1] == "[":
+                new.pop()
+            elif i == "}" and new and new[-1] == "{":
+                new.pop()
+            else:
+                return False
+        return len(new) == 0
+s = "()"
+sol = Solution()
+print(sol.isValid(s))
