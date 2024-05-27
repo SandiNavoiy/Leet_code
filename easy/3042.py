c51127e812426aba69d07237ab4ee374df092270
Sandi_Navoiy <santes.au@gmail.com>
2024-05-27 10:05:58 +0300

    612

---
+++ easy/3042.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPrefixSuffixPairs(self, words: list[str]) -> int:
+        ''''''
+        for i in range(len(words)):
+            for j in range(i+1,len(words)):
+                print(words[i])
+                print(words[j])
+                print(words[j][0:len(words[i])])
+                print(words[j][len(words[j])- len(words[i]):])
+                print("---")
+                if words[i] == words[j][0:len(words[i])] and words[i] == words[j][len(words[j])- len(words[i]):]:
+                    pass
+word = ["a","aba","ababa","aa"]
+s = Solution()
+print(s.countPrefixSuffixPairs(word))
