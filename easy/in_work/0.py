41bbe02d9d7ffdff70b8dfa10d42c129e9ff308c
Sandi_Navoiy <santes.au@gmail.com>
2024-05-29 23:11:58 +0300

    620+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,9 +1,54 @@
-class Solution:
-    def countPrimes(self, n: int) -> int:
-        """"""
-        new = []
+class Array:
+    """
+    Линейный статический массив.
+    """
 
+    def __init__(self, size):
+        # Данные массива, изначально массив пустой и все его элементы заполнены None.
+        # То есть сразу выделяем массив фиксированного объема.
+        self.data = [None] * size
 
-n = 10000
-s = Solution()
-print(s.countPrimes(n))
+        # Длина заполненного массива.
+        # По умолчанию 0, так как массив пустой.
+        self.length = 0
+
+        # Полный размер массива.
+        self.size = size
+
+    def append(self, value):
+        """
+        Добавление нового элемента в конец линейного массива.
+        Время работы O(1).
+        """
+        if self.length == self.size:
+            raise OverflowError
+        self.data[self.length] = value
+        self.length += 1
+
+    def remove(self, value):
+        """
+        Удаляет все элементы со значением value.
+        Время работы O(N).
+        """
+        while value in self.data:
+            v = self.data.index(value)
+            self.data.pop(v)
+            self.length -= 1
+            self.data.append(None)
+
+    def __str__(self):
+        """
+        Возвращает все элементы массива в виде строки.
+        """
+        return "[" + ", ".join(map(str, self.data[:self.length])) + "]"
+array = Array(5)
+array.append(6)
+array.append(2)
+array.append(1)
+array.append(2)
+array.append(9)
+print(array)
+#[6, 2, 1, 2, 9]
+array.remove(2)
+#[6, 1, 9]
+print(array)
