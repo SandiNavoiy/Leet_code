8448da4fb2cccd57b043f89b42bfd568fc383bdc
Sandi_Navoiy <santes.au@gmail.com>
2024-06-02 17:24:57 +0300

    624

---
+++ medium/784.py
@@ -0,0 +1,16 @@
+from itertools import  product
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> list[str]:
+        ''''''
+        new = []
+        for ch in s:
+            new.append([ch] if ch.isdigit() else [ch.lower(), ch.upper()])
+        print(new)
+
+        return [''.join(v) for v in product(*new)]
+
+
+s = "a1b2"
+sol = Solution()
+print(sol.letterCasePermutation(s))
