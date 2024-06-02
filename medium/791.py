46d116b5e5455907306a0f1131650e9a5d981b49
Sandi_Navoiy <santes.au@gmail.com>
2024-06-02 22:29:12 +0300

    625

---
+++ medium/791.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        new = []
+        ''''''
+        x = dict(Counter(s))
+        for i in order:
+            if i in s:
+                new.append(i*x[i] )
+                x[i]= 0
+        for k,v in x.items():
+            if v>0:
+                new.append(k*v)
+
+        return "".join(new)
+order = "cba"
+s = "abcd"
+sol = Solution()
+print(sol.customSortString(order, s))
