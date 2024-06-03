23674c724cc69ffcd2e513fa47565bb07c1263da
Sandi_Navoiy <santes.au@gmail.com>
2024-06-03 16:11:37 +0300

    627

---
+++ medium/162.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        ''''''
+        x = max(nums)
+        return nums.index(x)
+nums = [1,2,3,1]
+s = Solution()
+print(s.findPeakElement(nums))
+
