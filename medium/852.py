60223888c9ab0887d4947ebbe1f477bcc59e583a
Sandi_Navoiy <santes.au@gmail.com>
2024-06-03 16:08:21 +0300

    626

---
+++ medium/852.py
@@ -0,0 +1,8 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr:list[int]) -> int:
+        ''''''
+        x = max(arr)
+        return arr.index(x)
+arr = [0,2,1,0]
+s = Solution()
+print(s.peakIndexInMountainArray(arr))
