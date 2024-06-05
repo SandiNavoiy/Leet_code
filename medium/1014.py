ebf8c000727678c5a6cf5811f765e3abaaae314c
Sandi_Navoiy <santes.au@gmail.com>
2024-06-05 16:50:37 +0300

    630

---
+++ medium/1014.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: list[int]) -> int:
+        dp = [0] * (len(values))
+        dp[0] = values[0]
+        maxVal = 0
+
+        for i in range(1, len(values)):
+            dp[i] = max(dp[i - 1], values[i - 1] + i - 1)
+            maxVal = max(maxVal, dp[i] + values[i] - i)
+
+        return maxVal
+
+
+values = [8,1,5,2,6]
+s = Solution()
+print(s.maxScoreSightseeingPair(values))
