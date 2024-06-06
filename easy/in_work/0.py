64b65874b3585b45cee75d3a98d806be638e01fb
Sandi_Navoiy <santes.au@gmail.com>
2024-06-06 23:16:35 +0300

    635++

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,38 +1,98 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next_node = None
+class Array:
+    def __init__(self, size):
+        self.data = [None] * size
+        self.length = 0
+        self.size = size
+        self.ritr = 0
 
     def __str__(self):
-        return str(self.value)
-
-
-class Stack:
-    def __init__(self):
-        self.top = Node(None)
-
-    def pop(self):
         """
-        Извлекает элемент из стека.
+        Возвращает все элементы массива в виде строки.
         """
-        # Добавьте ваш код тут.
-
-    def push(self, value):
-        """
-        Добавляет элемент со значением value в стек.
-        """
-        next_node = Node(value)
+        return "[" + ", ".join(map(str, self.data[:self.length])) + "]"
 
 
-stack = Stack()
-stack.push(12)
-stack.push(7)
-stack.push(6)
-print(stack.pop())
-# 6
-print(stack.pop())
-# 7
-print(stack.pop())
-# 12
-print(stack.pop())
-None
+class Stack(Array):
+    """
+    Двойной стек на базе статического массива.
+    """
+
+    def pop_left(self):
+        """
+        Извлекает элемент из стека слева.
+        """
+        # Добавьте ваш код тут
+        if self.length - 1 < 0:
+            return None
+        x = self.data[self.length - 1]
+        self.data[self.length - 1] = None
+        self.length = self.length - 1
+        return x
+
+    def pop_right(self):
+        """
+        Извлекает элемент из стека справа.
+        """
+        # Добавьте ваш код тут
+        if self.ritr < 0:
+            return None
+        x = self.data[self.size - self.ritr -1]
+        self.data[self.size - self.ritr -1] = None
+        self.ritr = self.ritr - 1
+        return x
+
+    def push_left(self, value):
+        """
+        Добавляет элемент со значением value в стек слева.
+        """
+        # Добавьте ваш код тут
+        if self.length  + self.ritr < self.size:
+            self.data[self.length] = value
+            self.length = self.length + 1
+        else:
+            raise OverflowError
+
+    def push_right(self, value):
+        """
+        Добавляет элемент со значением value в стек справа.
+        """
+        # Добавьте ваш код тут
+        if self.length  + self.ritr < self.size:
+            self.data[self.size - self.ritr -1] = value
+            self.ritr = self.ritr + 1
+        else:
+            raise OverflowError
+
+    def clear(self):
+        """
+        Очищает стек.
+        """
+        self.data = [None] * self.size
+        self.length = 0
+
+        self.ritr = 0
+
+    def __str__(self):
+        """
+        Возвращает все элементы массива в виде строки.
+        Используем size, так как массив теперь заполняется с двух сторон.
+        """
+        return "[" + ", ".join(map(str, self.data[:self.size])) + "]"
+
+stack = Stack(6)
+stack.push_left(7)
+stack.push_left(6)
+stack.push_right(2)
+stack.push_right(1)
+stack.push_left(11)
+stack.push_right(8)
+print(stack)
+print(stack.pop_left())
+#11
+print(stack.pop_left())
+#6
+print(stack.pop_left())
+#7
+
+print(stack.pop_left())
+#2
