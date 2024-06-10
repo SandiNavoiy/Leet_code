a8a15a4cac28913ffb018a449cbd94ff484ec850
Sandi_Navoiy <santes.au@gmail.com>
2024-06-10 14:03:31 +0300

    641

---
+++ medium/1887.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reductionOperations(self, nums: list[int]) -> int:
+        ''''''
+        rez = 0
+        uniques = list(set(nums))
+        n = len(uniques)-1
+        uniques.sort(reverse=True)
+        d = {}
+        for i, j in enumerate(uniques):
+            d[j] = i
+        for i in nums:
+            rez = rez + (n) - d[i]
+
+        return rez
+nums = [1,1,2,2,3]
+
+s = Solution()
+print(s.reductionOperations(nums))
