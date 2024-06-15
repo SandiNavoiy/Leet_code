2f4276ca1fb287658c68cf6e72dad58ecf4eabe9
Alex <santes.au@gmail.com>
2024-06-15 11:18:50 +0300

    654

--- easy/203.py
+++ easy/203.py
@@ -6,11 +6,17 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head is None or head.node is None:
-            return head
+        while head is None and head.val == val:
+            head = head.next
+
         current = head
-        while current.node is not None:
-            if current.next.val  == val:
+        while current is not None and current.next is not None:
+            if current.next.val == val:
                 current.next = current.next.next
+            else:
+                current = current.next
+        return head
