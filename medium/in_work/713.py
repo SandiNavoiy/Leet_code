032781b5c56877b7d77403bc938703943b16dd0a
Alex <santes.au@gmail.com>
2024-06-17 08:37:03 +0300

    659

--- medium/in_work/713.py
+++ medium/in_work/713.py
@@ -1,17 +1,19 @@
-from functools import reduce
-
-
 class Solution:
     def numSubarrayProductLessThanK(self, nums: list[int], k: int) -> int:
         """"""
-        rez = []
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums) + 1):
-                if reduce(lambda a, b: a * b, nums[i:j]) < k:
-                    rez.append(nums[i:j])
-                else:
-                    break
-        return len(rez)
+        if k <= 1:
+            return 0
+        left = 0
+        proizv = 1
+        count = 0
+        for right in range(len(nums)):
+            proizv = proizv * nums[right]
+            while proizv >= k and left <= right:
+                proizv = proizv // nums[left]
+                left = left + 1
+            count = count + right - left + 1
+
+        return count
 
 
 nums = [10, 5, 2, 6]
