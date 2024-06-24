ad5a4294df2ae66ef0460185b91663630efd55f0
Sandi_Navoiy <santes.au@gmail.com>
2024-06-24 17:37:52 +0300

    664

---
+++ easy/3168.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumChairs(self, s: str) -> int:
+        ''''''
+        rez = 0
+        temp = 0
+        for i in s:
+            if i == "E":
+                temp = temp + 1
+                rez = max(rez, temp)
+            else:
+                temp = temp -1
+
+        return rez
+s = "ELEELEELLL"
+sol = Solution()
+print(sol.minimumChairs(s))
