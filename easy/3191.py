19effc5982130fd00bea35f17b0d7ce6f8436c1f
Sandi_Navoiy <santes.au@gmail.com>
2024-06-26 16:11:07 +0300

    668

---
+++ easy/3191.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minOperations(self, nums: list[int]) -> int:
+        ''''''
+        c = 0
+
+        n = len(nums)
+        rez = [1 for i in range(n)]
+        if nums == rez:
+            return c
+        for i in range(n-2):
+
+            if nums[i] == 0:
+                c = c + 1
+                nums[i] = 1
+
+                nums[i+1] = 1 - nums[i+1]
+
+                nums[i + 2] = 1 -nums[i + 2]
+
+        if nums == rez:
+            return c
+        return -1
+
+nums = [1,1,1]
+s = Solution()
+print(s.minOperations(nums))
