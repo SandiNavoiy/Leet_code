2f41dac8984f710c626096a58af027e798033d0e
Sandi_Navoiy <santes.au@gmail.com>
2024-06-30 14:54:13 +0300

    681

---
+++ medium/462.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMoves2(self, nums: list[int]) -> int:
+        ''''''
+        rez = 0
+        nums.sort()
+        mediana = nums[len(nums)//2]
+        for i in nums:
+            if i == mediana:
+                continue
+            rez = rez  + abs(mediana - i)
+
+        return rez
+
+
+
+nums = [1,10,2,9]
+s = Solution()
+print(s.minMoves2(nums))
