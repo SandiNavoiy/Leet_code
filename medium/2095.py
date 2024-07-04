3e3e1d9a7e1f81e6f349ee5a8bf6dd565884b026
Sandi_Navoiy <santes.au@gmail.com>
2024-07-04 23:19:01 +0300

    698

---
+++ medium/2095.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ''''''
+        cur = head
+        fin = head
+        pref = head
+        if cur.next is None:
+            return None
+        while fin and fin.next:
+            pref = cur
+            cur = cur.next
+            fin = fin.next.next
+        pref.next = cur.next
+        return head
