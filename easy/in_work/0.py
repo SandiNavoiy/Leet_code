b4140c3961aa3a11faf3e535f157c71335de1a38
Alex <santes.au@gmail.com>
2024-07-08 11:59:54 +0300

    700+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,21 +1,48 @@
-def bubblesort(array):
+def rotate_letter(letter: str, shift: int) -> str:
     """
-    Алгоритм пузырьковой сортировки массива в прямом порядке.
+    Функция сдвигает символ letter на shift позиций.
+
+    Аргументы:
+    letter (str): Одна английская буква в нижнем регистре.
+    shift (int): Значение сдвига буквы. Может быть положительным или отрицательным.
+
+    Возвращает:
+    str: Новая буква после сдвига.
     """
+    if not letter.islower() or len(letter) != 1:
+        raise ValueError(
+            "Параметр letter должен быть одной английской буквой в нижнем регистре."
+        )
 
-    # Модифицируйте алгоритм.
-    n = 1
-    is_sorted = False
-    while not is_sorted:
-        is_sorted = True
+    # Приводим сдвиг к значению в пределах 0-25
+    shift = shift % 26
 
-        for i in range(len(array) - n):
-            if array[i] < array[i + 1]:
-                array[i], array[i + 1] = array[i + 1], array[i]
-                is_sorted = False
+    # Преобразуем букву в числовое значение (0-25), сдвигаем и обратно преобразуем в букву
+    new_letter_code = ord(letter) - ord("a")  # преобразуем в число 0-25
+    new_letter_code = (new_letter_code + shift) % 26  # сдвигаем и берем по модулю
+    new_letter = chr(new_letter_code + ord("a"))  # обратно преобразуем в букву
 
-        n += 1
-    return array
+    return new_letter
 
 
-print(bubblesort([1,5,9,7,2,7,0,4,3]))
+def caesar_cipher(phrase: str, key: int) -> str:
+    """Шифр Цезаря"""
+    encrypted_phrase = []
+
+    for char in phrase:
+        if char.isalpha():  # если символ является буквой
+            encrypted_char = (
+                rotate_letter(char.lower(), key)
+                if char.islower()
+                else rotate_letter(char.lower(), key).upper()
+            )
+            encrypted_phrase.append(encrypted_char)
+        else:  # если символ является знаком пунктуации или пробелом
+            encrypted_phrase.append(char)
+
+    return "".join(encrypted_phrase)
+
+
+print(caesar_cipher.__annotations__)
+print(caesar_cipher.__doc__)
+print(caesar_cipher("lost in the echo", 1))
