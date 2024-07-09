1000d295ab1cafa67f52be8b7e08971921c72b9a
Alex <santes.au@gmail.com>
2024-07-09 12:22:59 +0300

    702+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,48 +1,10 @@
-def rotate_letter(letter: str, shift: int) -> str:
-    """
-    Функция сдвигает символ letter на shift позиций.
-
-    Аргументы:
-    letter (str): Одна английская буква в нижнем регистре.
-    shift (int): Значение сдвига буквы. Может быть положительным или отрицательным.
-
-    Возвращает:
-    str: Новая буква после сдвига.
-    """
-    if not letter.islower() or len(letter) != 1:
-        raise ValueError(
-            "Параметр letter должен быть одной английской буквой в нижнем регистре."
-        )
-
-    # Приводим сдвиг к значению в пределах 0-25
-    shift = shift % 26
-
-    # Преобразуем букву в числовое значение (0-25), сдвигаем и обратно преобразуем в букву
-    new_letter_code = ord(letter) - ord("a")  # преобразуем в число 0-25
-    new_letter_code = (new_letter_code + shift) % 26  # сдвигаем и берем по модулю
-    new_letter = chr(new_letter_code + ord("a"))  # обратно преобразуем в букву
-
-    return new_letter
+lines = [(5, 6), (5, 4), (1, 0), (0, -1), (1, 2), (2, 1)]
 
 
-def caesar_cipher(phrase: str, key: int) -> str:
-    """Шифр Цезаря"""
-    encrypted_phrase = []
-
-    for char in phrase:
-        if char.isalpha():  # если символ является буквой
-            encrypted_char = (
-                rotate_letter(char.lower(), key)
-                if char.islower()
-                else rotate_letter(char.lower(), key).upper()
-            )
-            encrypted_phrase.append(encrypted_char)
-        else:  # если символ является знаком пунктуации или пробелом
-            encrypted_phrase.append(char)
-
-    return "".join(encrypted_phrase)
+def get_sort_lines(l):
+    l = sorted(l, key=lambda x: (abs(x[1] - x[0]), x[0], x[1]))
+    return l
 
 
-print(caesar_cipher.__annotations__)
-print(caesar_cipher.__doc__)
-print(caesar_cipher("lost in the echo", 1))
+print(get_sort_lines(lines))
+# [(-1, -2), (1, 0), (2, 3), (5, 4)]
