e88a32ac1cb7eaf06b98dbe45c1806836e9c1f55
Sandi_Navoiy <santes.au@gmail.com>
2024-07-17 23:29:51 +0300

    719

---
+++ medium/633.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        ''''''
+        start = 0
+        end  = int(c**0.5)
+        while start <= end:
+            if start**2 + end**2 == c:
+                return True
+            elif start**2 + end**2 > c:
+                end = end - 1
+            elif start**2 + end**2 < c:
+                start = start + 1
+
+        return False
+c = 2
+s = Solution()
+print(s.judgeSquareSum(c))
