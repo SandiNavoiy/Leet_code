4e28c7b304ed4dfb1075634504201364938b2536
Sandi_Navoiy <santes.au@gmail.com>
2024-07-19 12:17:27 +0300

    725

---
+++ medium/973.py
@@ -0,0 +1,12 @@
+import math
+
+
+class Solution:
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        ''''''
+        points.sort(key=lambda x: math.dist (x, [0,0] ))
+        return points[0:k]
+points = [[1,3],[-2,2]]
+k = 1
+s = Solution()
+print(s.kClosest(points, k))
