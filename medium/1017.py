4d6bd811c0f5fa3fa0e3d644d47cc82d3ad45d74
Sandi_Navoiy <santes.au@gmail.com>
2024-07-20 16:29:07 +0300

    727

---
+++ medium/1017.py
@@ -0,0 +1,19 @@
+class Solution:
+    def baseNeg2(self, n: int) -> str:
+        if n==0:
+            return '0'
+
+        ret = []
+        while (n != 0):
+
+            if n % 2 == 0:
+                ret.append('0')
+                n //= -2
+            else:
+                ret.append('1')
+                n = (n - 1) // -2
+
+        return ''.join(ret[::-1])
+n = 2
+s = Solution()
+print(s.baseNeg2(n))
