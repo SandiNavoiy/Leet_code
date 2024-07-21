fa0733bb32f23c2df35984405a60e82993e7ec77
Sandi_Navoiy <santes.au@gmail.com>
2024-07-21 20:49:45 +0300

    733

---
+++ medium/2294.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partitionArray(self, nums: list[int], k: int) -> int:
+        ''''''
+        nums.sort()
+        if k == 0:
+            return len(set(nums))
+        if nums[-1] - nums[0] <= k:
+            return 1
+        v = 0
+        temp = nums[0]
+
+        for i in range(len(nums)):
+            if nums[i] - temp > k:
+                temp = nums[i]
+                v = v + 1
+
+        return v + 1
+
+
+nums = [3,6,1,2,5]
+k = 2
+s = Solution()
+print(s.partitionArray(nums, k))
