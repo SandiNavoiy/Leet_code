0039386965eb1532533976ee54140e929488905e
Sandi_Navoiy <santes.au@gmail.com>
2024-07-25 23:50:25 +0300

    739

---
+++ medium/148.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ''''''
+        if head == None:
+            return None
+        temp_arr = []
+        temp = head
+        while temp is  not None:
+            temp_arr.append(temp.val)
+            temp = temp.next
+
+        temp_arr.sort()
+        temp = head
+        i = 0
+
+        while temp is not None:
+            temp.val = temp_arr[i]
+
+            i = i + 1
+            temp = temp.next
+        return head
