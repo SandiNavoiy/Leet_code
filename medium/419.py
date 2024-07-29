755c475ef580f3cc2d034ba8983edcf83b13a8af
Sandi_Navoiy <santes.au@gmail.com>
2024-07-29 13:48:10 +0300

    747

--- medium/419.py
+++ medium/419.py
@@ -1,12 +1,18 @@
 class Solution:
     def countBattleships(self, board: list[list[str]]) -> int:
         """"""
+        count = 0
         m = len(board)
         n = len(board[0])
         for i in range(m):
             for j in range(n):
                 if board[i][j] == "X":
-                    print("ok")
+                    flag = 1
+                    if (i > 0 and board[i-1] [j] == "X") or (j > 0 and board[i][j-1]== "X"):
+                        flag = 0
+                    count = count + flag
+
+        return count
 
 
 board = [["X", ".", ".", "X"], [".", ".", ".", "X"], [".", ".", ".", "X"]]
