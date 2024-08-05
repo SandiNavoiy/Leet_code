1dd620ebb2873e1da73304f2162db5c6f49d1020
Sandi_Navoiy <santes.au@gmail.com>
2024-08-05 15:48:47 +0300

    758

---
+++ easy/455.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findContentChildren(self, g: list[int], s: list[int]) -> int:
+        g.sort()
+        s.sort()
+
+        i, j = 0, 0
+        content_children = 0
+
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                content_children += 1
+                i += 1
+            j += 1
+
+        return content_children
