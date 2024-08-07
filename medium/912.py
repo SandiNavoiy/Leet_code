e79abe3bb6052169969103c73a18d3da2b9a564b
Sandi_Navoiy <santes.au@gmail.com>
2024-08-07 11:42:34 +0300

    766

---
+++ medium/912.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        ''''''
+        nums.sort()
+        return nums
+nums = [5,2,3,1]
+s = Solution()
+print(s.sortArray(nums))
