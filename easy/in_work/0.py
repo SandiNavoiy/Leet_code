c5da8bc369448868a3cbcc35ba2ab1c4e9d2d08f
Alex <santes.au@gmail.com>
2024-08-11 12:04:17 +0300

    769

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,8 +1,43 @@
-def summa(param):
-    if param == 1:
-        return param
+class Node:
+    """
+    Класс узла дерева.
+    """
+
+    def __init__(self, key, value):
+        # Ключ.
+        self.key = key
+        # Значение.
+        self.value = value
+        # Потомки.
+        self.children = []
+
+
+def find(node, x):
+    if node.key == x:
+        return node.value
     else:
-        return param + summa(param - 1)
+        for child in node.children:
+            result = find(child, x)
+            if result is not None:
+                return result
+
+            # Если узел с таким ключом не найден.
+    return None
 
 
-print(summa(4))
+# Создаём дерево
+root = Node("a", "A")
+node_b = Node("b", "B")
+node_c = Node("c", "C")
+node_d = Node("d", "D")
+node_i = Node("i", "I")
+
+root.children.extend([node_b, node_c, node_d])
+node_b.children.extend([Node("e", "E")])
+node_c.children.extend([Node("f", "F"), Node("g", "K")])
+node_d.children.extend([Node("h", "H"), node_i, Node("k", "K")])
+node_i.children.extend([Node("l", "L"), Node("m", "M"), Node("n", "N"), Node("o", "O")])
+
+# Ищем элемент с ключом m
+result = find(root, "m")
+print(result)
