f5e57d9decbb0d56f59eb48c15bc9f0373235682
Alex <santes.au@gmail.com>
2024-08-11 12:04:23 +0300

    769

--- easy/100.py
+++ easy/100.py
@@ -15,4 +15,8 @@ class Solution(object):
             return True
         if not p or not q:
             return False
-        return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        return (
+            p.val == q.val
+            and self.isSameTree(p.left, q.left)
+            and self.isSameTree(p.right, q.right)
+        )
