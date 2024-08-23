88887fad749dddd44ceb32098900c8264ef53e72
Alex <santes.au@gmail.com>
2024-08-23 14:35:21 +0300

    771+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,28 +1,20 @@
-# Definition for singly-linked list.
-from typing import Optional
+def quick_power(a, n):
+    # Выводим текущее состояние параметров
+    print(f"State: a={a}, n={n}")
+
+    # Базовый случай: любое число в нулевой степени равно 1
+    if n == 0:
+        return 1
+
+    # Если степень n четная
+    if n % 2 == 0:
+        return quick_power(a, n // 2) ** 2
+
+    # Если степень n нечетная
+    else:
+        return a * quick_power(a, n - 1)
 
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-class Solution:
-    def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
-        """"""
-        new = []
-        for i in lists:
-            cur = i
-            while cur:
-                new.append(cur.val)
-                cur = cur.next
-        new.sort()
-        head = ListNode()
-        cur = head
-
-        for i in new:
-            cur.next = ListNode(i)
-            cur = cur.next
-
-        return head.next
+# Примеры использования
+print(quick_power(2, 10))  # Должно вывести 1024
+print(quick_power(3, 5))  # Должно вывести 243
