09c9e47e56a7127e97cda62d819b46bcbcf2a2af
Sandi_Navoiy <santes.au@gmail.com>
2024-08-26 09:28:31 +0300

    772

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,20 +1,43 @@
-def quick_power(a, n):
-    # Выводим текущее состояние параметров
-    print(f"State: a={a}, n={n}")
+class Node:
+    """
+    Класс узла дерева.
+    """
 
-    # Базовый случай: любое число в нулевой степени равно 1
-    if n == 0:
-        return 1
+    def __init__(self, key, value):
+        # Ключ.
+        self.key = key
+        # Значение.
+        self.value = value
+        # Потомки.
+        self.children = []
 
-    # Если степень n четная
-    if n % 2 == 0:
-        return quick_power(a, n // 2) ** 2
 
-    # Если степень n нечетная
+def find(node, x):
+    if node.key == x:
+        return node.value
     else:
-        return a * quick_power(a, n - 1)
+        for child in node.children:
+            result = find(child, x)
+            if result is not None:
+                return result
+
+            # Если узел с таким ключом не найден.
+    return None
 
 
-# Примеры использования
-print(quick_power(2, 10))  # Должно вывести 1024
-print(quick_power(3, 5))  # Должно вывести 243
+# Создаём дерево
+root = Node("a", "A")
+node_b = Node("b", "B")
+node_c = Node("c", "C")
+node_d = Node("d", "D")
+node_i = Node("i", "I")
+
+root.children.extend([node_b, node_c, node_d])
+node_b.children.extend([Node("e", "E")])
+node_c.children.extend([Node("f", "F"), Node("g", "K")])
+node_d.children.extend([Node("h", "H"), node_i, Node("k", "K")])
+node_i.children.extend([Node("l", "L"), Node("m", "M"), Node("n", "N"), Node("o", "O")])
+
+# Ищем элемент с ключом m
+result = find(root, "m")
+print(result)
