f3501b027f201611dcf2a70df08abd107026e02e
Alex <santes.au@gmail.com>
2024-09-06 13:45:50 +0300

    777+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,15 +1,40 @@
-def flatten_dict(lst):
-    t = {}
-    for k,v in lst.items():
-        if isinstance(v, dict):
-            flat = flatten_dict(v)
-            for sub_k, sub_v in flat.items():
-                t[k + '_' + sub_k] = sub_v
-        else:
-            t[k] = v
-        print(t)
-    return t
+import functools
 
 
+def counting_calls(func):
+    func.call_count = 0  # Инициализация счетчика вызовов
+    func.calls = []  # Инициализация списка для хранения переданных аргументов
 
-print(flatten_dict({'Q': {'w': {'E': {'r': {'T': {'y': 123}}}}}}))
+    @functools.wraps(func)  # Сохраняет метаданные оригинальной функции
+    def wrapper(*args, **kwargs):
+        # Увеличиваем счетчик вызовов
+        wrapper.call_count += 1
+
+        # Сохраняем аргументы в виде словаря
+        wrapper.calls.append({"args": args, "kwargs": kwargs})
+
+        # Вызываем оригинальную функцию и возвращаем результат
+        return func(*args, **kwargs)
+
+    # Передаем атрибуты в обертку, а не в оригинальную функцию
+    wrapper.call_count = func.call_count
+    wrapper.calls = func.calls
+
+    return wrapper
+
+
+@counting_calls
+def add(a: int, b: int) -> int:
+    """Возвращает сумму двух чисел"""
+    return a + b
+
+
+print(add.__name__)
+print(add.__doc__)
+
+print(add(10, b=20))
+print("Количество вызовов =", add.call_count)
+print(add.calls)
+
+print(add(5, 6))
+print(add.calls[1])
