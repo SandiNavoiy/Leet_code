a54b2b93d06edd108006bf2cf07dae84517f6b8d
Sandi_Navoiy <santes.au@gmail.com>
2024-09-15 22:38:06 +0300

    779+

--- medium/718.py
+++ medium/718.py
@@ -1,7 +1,7 @@
 class Solution:
     def findLength(self, nums1: list[int], nums2: list[int]) -> int:
         ''''''
-        dp = [[0]* (len(nums1)+1) for i in range(len(nums2) + 1)]
+        dp = [[0]* (len(nums2)+1) for i in range(len(nums1) + 1)]
         for i in range(len(nums1)):
             for j in range(len(nums2)):
                 if nums1[i] == nums2[j]:
