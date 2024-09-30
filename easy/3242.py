b42ff25dc1af5ae31114403c978dbb3f0682eca0
Alex <santes.au@gmail.com>
2024-09-30 13:13:28 +0300

    788

---
+++ easy/3242.py
@@ -0,0 +1,48 @@
+class NeighborSum:
+
+    def __init__(self, grid: List[List[int]]):
+        self.grid = grid
+        self.l = len(grid)
+
+    def get_position_of_value(self, value):
+        row, col = 0, 0
+        for i in range(self.l):
+            if value in self.grid[i]:
+                row = i
+                col = self.grid[i].index(value)
+                return row, col
+
+    def adjacentSum(self, value: int) -> int:
+        row, col = self.get_position_of_value(value)
+        s = 0
+        if row + 1 != self.l:
+            s += self.grid[row + 1][col]
+        if row - 1 != -1:
+            s += self.grid[row - 1][col]
+        if col - 1 != -1:
+            s += self.grid[row][col - 1]
+        if col + 1 != self.l:
+            s += self.grid[row][col + 1]
+        return s
+
+    def diagonalSum(self, value: int) -> int:
+        row, col = self.get_position_of_value(value)
+        s = 0
+        if row + 1 != self.l and col + 1 != self.l:
+            s += self.grid[row + 1][col + 1]
+        if row + 1 != self.l and col - 1 != -1:
+            s += self.grid[row + 1][col - 1]
+        if row - 1 != -1 and col + 1 != self.l:
+            s += self.grid[row - 1][col + 1]
+        if row - 1 != -1 and col - 1 != -1:
+            s += self.grid[row - 1][col - 1]
+        return s
+# Your NeighborSum object will be instantiated and called as such:
+# obj = NeighborSum(grid)
+# param_1 = obj.adjacentSum(value)
+# param_2 = obj.diagonalSum(value)
+
+# Your NeighborSum object will be instantiated and called as such:
+# obj = NeighborSum(grid)
+# param_1 = obj.adjacentSum(value)
+# param_2 = obj.diagonalSum(value)
