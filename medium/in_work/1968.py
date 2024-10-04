8176e3558e2fbfbd6483e26cf5de85d58a3655f8
Alex <santes.au@gmail.com>
2024-10-04 13:06:11 +0300

    799(1968)

---
+++ medium/in_work/1968.py
@@ -0,0 +1,18 @@
+from statistics import median
+
+
+class Solution:
+    def rearrangeArray(self, nums: list[int]) -> list[int]:
+        """"""
+        med = median(nums)
+        low = [i for i in nums if i < med]
+        high = [i for i in nums if i >= med]
+
+
+
+        return high
+
+
+nums = [1, 2, 3, 4, 5]
+s = Solution()
+print(s.rearrangeArray(nums))
