9d11dd171cba481858f8ff5b2890590e80a42dab
Alex <santes.au@gmail.com>
2024-10-06 09:19:41 +0300

    800

--- medium/in_work/1968.py
+++ medium/in_work/1968.py
@@ -4,11 +4,17 @@ from statistics import median
 class Solution:
     def rearrangeArray(self, nums: list[int]) -> list[int]:
         """"""
+        ans = []
         med = median(nums)
         low = [i for i in nums if i < med]
         high = [i for i in nums if i >= med]
+        while low or high:
+            if low:
+                ans.append(low.pop())
+            if high:
+                ans.append(high.pop())
 
-        return high
+        return ans
 
 
 nums = [1, 2, 3, 4, 5]
