3207da30b403f305508dddc2a160c94a68757460
Alex <santes.au@gmail.com>
2024-10-08 11:33:07 +0300

    807

--- medium/2761.py
+++ medium/2761.py
@@ -1,19 +1,19 @@
 class Solution:
     def findPrimePairs(self, n: int) -> List[List[int]]:
-        prime=[0]*(n+1)
-        prime[0]=1
-        prime[1]=1
-        for x in range(2,n+1):
-            if prime[x]==0:
-                i=x
-                while x*i<=n:
-                    if prime[x*i]==0:
-                        prime[x*i]=1
-                    i+=1
-        result=[]
-        for x in range(2,((n//2)+1)):
-            if prime[x]==0 and prime[n-x]==0:
-                result.append([x,n-x])
+        prime = [0] * (n + 1)
+        prime[0] = 1
+        prime[1] = 1
+        for x in range(2, n + 1):
+            if prime[x] == 0:
+                i = x
+                while x * i <= n:
+                    if prime[x * i] == 0:
+                        prime[x * i] = 1
+                    i += 1
+        result = []
+        for x in range(2, ((n // 2) + 1)):
+            if prime[x] == 0 and prime[n - x] == 0:
+                result.append([x, n - x])
         return result
 
 
