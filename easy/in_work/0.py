58d54b978a31bd160ec907d6b4bb78b993f0f83c
Alex <santes.au@gmail.com>
2024-10-09 15:47:46 +0300

    810+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,11 +1,16 @@
-def replace(text, old, new=""):
-    ee = ""
-    for i in text:
-        if i == old:
-            ee += new
-        else:
-            ee += i
-    return ee
+def get_average():
+    total = 0
+    count = 0
+    while True:
+        number = (
+            yield total / count if count > 0 else 0
+        )  # Возвращаем среднее, если есть числа
+        total += number
+        count += 1
 
 
-print(replace("Bella Ciao", "a"))
+coro = get_average()
+next(coro)
+print(coro.send(10))
+print(coro.send(20))
+print(coro.send(6))
