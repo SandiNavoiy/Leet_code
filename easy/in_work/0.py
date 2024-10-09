ceee8878043f59985db085b1c46f5703145f5103
Alex <santes.au@gmail.com>
2024-10-09 16:43:35 +0300

    810+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,16 +1,1 @@
-def get_average():
-    total = 0
-    count = 0
-    while True:
-        number = (
-            yield total / count if count > 0 else 0
-        )  # Возвращаем среднее, если есть числа
-        total += number
-        count += 1
-
-
-coro = get_average()
-next(coro)
-print(coro.send(10))
-print(coro.send(20))
-print(coro.send(6))
+class jyyg
