b54cf7c40b72d3a7bb85c07d8802915fec17d0d3
Alex <santes.au@gmail.com>
2024-10-10 13:17:18 +0300

    810+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,1 +1,15 @@
-class jyyg
+class Car:
+    model = "BMW"
+    engine = 1.6
+
+    @staticmethod
+    def drive():
+        print("Let's go")
+
+
+a = Car()
+b = Car()
+Car.drive()
+getattr(Car, "drive")()
+# a.drive()
+# b.drive()
