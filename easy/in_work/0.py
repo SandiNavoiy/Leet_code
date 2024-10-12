7718e8ee6442bd3cfcfb6f023da7af669fc05f48
Sandi_Navoiy <santes.au@gmail.com>
2024-10-13 00:01:48 +0300

    811

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,15 +1,1 @@
-class Car:
-    model = "BMW"
-    engine = 1.6
-
-    @staticmethod
-    def drive():
-        print("Let's go")
-
-
-a = Car()
-b = Car()
-Car.drive()
-getattr(Car, "drive")()
-# a.drive()
-# b.drive()
+class jyyg
