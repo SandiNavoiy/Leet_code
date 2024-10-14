28a5df61a178f934206a0dc2138c178e389daedb
Alex <santes.au@gmail.com>
2024-10-14 15:04:20 +0300

    810+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,43 +1,34 @@
-class Point:
-    def __init__(self):
-        self.x = None
-        self.y = None
-
-    def set_coordinates(self, x, y):
-        self.x = x
-        self.y = y
-
-    def get_distance(self, object):
-        if isinstance(object, Point):
-            distance = ((self.x - object.x) ** 2 + (self.y - object.y) ** 2) ** 0.5
-            return distance
-        else:
-            print("Передана не точка")
-            return None
+# Напишите определение класса Person
 
 
-# Код ниже не удаляйте, он нужен для проверок
-p1 = Point()
-p2 = Point()
-assert isinstance(p1, Point)
-assert isinstance(p2, Point)
+# Ниже код для проверки методов класса Person
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-p1.set_coordinates(1, 2)
-assert p1.x == 1
-assert p1.y == 2
-p2.set_coordinates(4, 6)
-assert p2.x == 4
-assert p2.y == 6
-assert p1.get_distance(p2) == 5.0
-p3 = Point()
-p3.set_coordinates(10, 10)
-p1.set_coordinates(4, 2)
-assert p1.get_distance(p3) == 10.0
-res = p1.get_distance(10)  # Распечатает "Передана не точка", вернет None
-assert (
-    res is None
-), "Метод get_distance должен возвращать None, если в него была передана не точка"
+    def full_name(self):
+        return f"{self.last_name} {self.first_name}"
 
-assert (
-    p2.get_distance([1, 2, 3]) is None
-)  # Распечатает "Передана не точка", вернет None
+    def is_adult(self):
+        return self.age >= 18
+
+
+p1 = Person("Ash", "Ketchum", 20)
+assert isinstance(p1, Person)
+
+assert p1.full_name() == "Ketchum Ash"
+assert p1.age == 20
+assert p1.first_name == "Ash"
+assert p1.last_name == "Ketchum"
+assert p1.is_adult() is True
+
+p2 = Person("Hermione", "Granger", 16)
+assert isinstance(p2, Person)
+assert p2.age == 16
+assert p2.first_name == "Hermione"
+assert p2.last_name == "Granger"
+assert p2.full_name() == "Granger Hermione"
+assert p2.is_adult() is False
+print("Good")
