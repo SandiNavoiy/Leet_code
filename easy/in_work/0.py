06c39ac671f394971041850c1068c1985fb70a29
Sandi_Navoiy <santes.au@gmail.com>
2024-10-17 23:23:26 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,31 +1,43 @@
-class Date:
-    def __init__(self, date, month, year):
-        self.__date = f"{month}/{date}/{year}"
+# Напишите определение класса Password       
+
+
+# Ниже код для проверки методов класса Password
+
+class Password:
+    def __init__(self, password):
+        self.__password = password
 
     @property
-    def date(self):
-        return self.__date
+    def password(self):
+        return self.__password
 
+    @password.setter
+    def password(self, new_password):
+        self.__password = new_password
     @property
-    def usa_date(self):
-        return f"{self.month}-{self.day}-{self.year}"
+    def strength(self):
+        if len(self.__password) < 8:
+            return "Weak"
+        elif len(self.__password) < 12:
+            return "Medium"
+        else:
+            return "Strong"
 
 
-d1 = Date(5, 10, 2001)
-assert isinstance(d1, Date)
-assert d1.date == "05/10/2001"
-# assert d1.usa_date == "10-05-2001"
-# assert isinstance(type(d1).date, property), "Вы не создали property date"
-# print(d1.date, d1.usa_date)
-#
-# d2 = Date(15, 3, 999)
-# assert isinstance(d2, Date)
-# assert d2.date == "15/03/0999"
-# assert d2.usa_date == "03-15-0999"
-# assert isinstance(type(d2).date, property), "Вы не создали property date"
-# print(d2.date, d2.usa_date)
-#
-# d3 = Date(3, 5, 3)
-# assert d3.date == "03/05/0003"
-# assert d3.usa_date == "05-03-0003"
-# print(d3.date, d3.usa_date)
+
+pass_1 = Password("Alligator34")
+assert pass_1.password == "Alligator34"
+assert pass_1.strength == "Medium"
+assert len(pass_1.__dict__) == 1, 'У ЭК должен храниться только один атрибут'
+
+pass_2 = Password("Alligator345678")
+assert pass_2.password == "Alligator345678"
+assert pass_2.strength == "Strong"
+pass_1.password = "123"
+assert pass_1.strength == "Weak"
+assert len(pass_2.__dict__) == 1, 'У ЭК должен храниться только один атрибут'
+
+pass_3 = Password("345678")
+assert pass_3.strength == "Weak"
+print('Good')
+assert len(pass_3.__dict__) == 1, 'У ЭК должен храниться только один атрибут'
