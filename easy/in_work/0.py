095a76b94854e2d52f72c44e19adb60f80344e4d
Sandi_Navoiy <santes.au@gmail.com>
2024-10-18 23:14:38 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,15 +1,36 @@
-class MyClass:
-    class_attribute = "I am a class attribute"
+class User:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+
+user1 = User('batya99', 'admin')
+
+
+class Access:
+
+    __access_list= ['admin', 'developer']
 
     @staticmethod
-    def change(new_value):
-        MyClass.class_attribute = new_value
+    def __check_access(role):
+        if role in Access.__access_list:
+            return True
+        else:
+            return False
+    @staticmethod
+    def get_access(user):
+        if isinstance(user, User):
+            if Access.__check_access(user.role):
+                print(f"User {user.name}: success")
+            else:
+                print("AccessDenied")
+        else:
+            print("AccessTypeError")
 
 
-example_1 = MyClass()
-example_2 = MyClass()
+Access.get_access(user1) # печатает "User batya99: success"
 
-example_1.change("Class attribute modified")
+zaya = User('milaya_zaya999', 'user')
+Access.get_access(zaya) # печатает AccessDenied
 
-print(example_1.class_attribute)
-print(example_2.class_attribute)
+Access.get_access(5) # печатает AccessTypeError
