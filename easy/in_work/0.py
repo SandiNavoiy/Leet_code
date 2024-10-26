13d394cb1c3bf35fd9d3c51f264b354e8c28732d
Sandi_Navoiy <santes.au@gmail.com>
2024-10-26 23:14:09 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,21 +1,37 @@
-class PrettyPrint:
-    def __str__(self):
-        return f"{self.__class__.__name__}({', '.join([f'{i}={self.__dict__[i]}' for i in self.__dict__])})"
-
-
-class Student(PrettyPrint):
-    def __init__(self, name, surname, student_id, faculty, specialty):
-        self.student_id = student_id
+# Напишите определение классов Person Company Employee
+class Person:
+    def __init__(self, name: str, age: int):
         self.name = name
-        self.surname = surname
-        self.faculty = faculty
-        self.specialty = specialty
+        self.age = age
 
-    def __str__(self):
-        return f"{self.name} {self.surname}"
+    def display_person_info(self):
+        print(f"Person: {self.name}, {self.age}")
 
+class Company:
+    def __init__(self, company_name : str, location: str):
+        self.company_name = company_name
+        self.location = location
 
-student_1 = Student("Иван", "Иванов", 12345, "Физический", "Математика")
-student_2 = Student("Анна", "Смирнова", 67890, "Химический", "Биология")
-print(student_1)
-print(student_2)
+    def display_company_info(self):
+        print(f"Company: {self.company_name}, {self.location}")
+
+# Ниже расположены проверки для кода
+
+class Employee(Person, Company):
+    def __init__(self, name: str, age: int, company_name: str, location: str):
+        super().__init__(name, age)
+        Company.__init__(self, company_name,location)
+
+a = Person('Ivan', 32)
+a.display_person_info()
+
+a = Company('Zara', 'Prague')
+a.display_company_info()
+#
+emp = Employee('Jessica', 28, 'Google', 'Atlanta')
+emp.display_person_info()
+emp.display_company_info()
+#
+emp2 = Employee('Kolya', 11, 'Facebook', 'Seatle')
+emp2.display_person_info()
+emp2.display_company_info()
