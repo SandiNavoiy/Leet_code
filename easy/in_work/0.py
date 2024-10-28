052ea07dc03d448d8a5cd7c92cd3698a6fae6fc7
Sandi_Navoiy <santes.au@gmail.com>
2024-10-28 17:15:56 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,6 +1,57 @@
-def func(phrase):
-    print(phrase)
-    func(phrase)
+# Создайте классы Database MySQLDatabase и PostgreSQLDatabase
+from abc import ABC, abstractmethod
 
 
-func("Это рекурсия, детка!")
+class Database(ABC):
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def execute(self, query):
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        pass
+
+class MySQLDatabase(Database):
+
+    def connect(self):
+        print("Connecting to MySQL database...")
+
+
+    def execute(self, query):
+        print(f"Executing query '{query}' in MySQL database...")
+
+
+    def disconnect(self):
+        print("Disconnecting from MySQL database...")
+
+class PostgreSQLDatabase(Database):
+
+    def connect(self):
+        print("Connecting to PostgreSQL database...")
+
+
+    def execute(self, query):
+        print(f"Executing query '{query}' in PostgreSQL database...")
+
+
+    def disconnect(self):
+        print("Disconnecting from PostgreSQL database...")
+
+# Код для проверки
+
+mysql_db = MySQLDatabase()
+postgresql_db = PostgreSQLDatabase()
+
+mysql_db.connect()
+mysql_db.execute(
+    "SELECT * FROM customers;")
+mysql_db.disconnect()
+
+postgresql_db.connect()
+postgresql_db.execute(
+    "SELECT * FROM customers;")
+postgresql_db.disconnect()
