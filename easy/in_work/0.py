cb08357f090989fa3adb8c60efca0e1591e7a504
Alex <santes.au@gmail.com>
2024-10-30 17:45:11 +0300

    813+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,19 +1,53 @@
-class SequenceIterator:
+class StackIterator:
     def __init__(self, value):
-        self.value = value[::2] + value[1::2]
+        self.value = value
         self.index = 0
 
-    def __iter__(self):
-        return self
     def __next__(self):
-        if self.index == len(self.value):
-            raise StopIteration
+        # if self.index == len(self.value):
+        #     raise StopIteration
         t = self.value[self.index]
         self.index += 1
         return t
 
-#
-container = SequenceIterator([1, 5, 4, 6, 43, True, 'hello'])
 
-for i in container:
-    print(i)
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if len(self.items) == 0:
+            print("Empty Stack")
+        else:
+            return self.items.pop()
+
+    def peek(self):
+        if len(self.items) == 0:
+            print("Empty Stack")
+        else:
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+    def __iter__(self):
+        return StackIterator(self)
+
+
+stack = Stack()
+
+stack.push(100)
+stack.push(True)
+stack.push("hello")
+stack.push("world")
+
+# Используем итератор для обхода стека
+for item in stack:
+    print(item)
+#
