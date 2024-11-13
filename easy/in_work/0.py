6667d5d6e3717bacc7de40c7caafbe9ddd836fda
Alex <santes.au@gmail.com>
2024-11-13 11:57:26 +0300

    815

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,12 +1,26 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Vector:
+    def __init__(self, *args):
+        self.values = list(args)
 
-    def __str__(self):
-        print(f"Point(x={self.x},y={self.y})")
+    def __repr__(self):
+        return f'Vector({", ".join([str(value) for value in self.values])})'
+
+    def __getitem__(self, item):
+        if isinstance(item, str):
+            item = len(item)
+
+        if 1 <= item <= len(self.values):
+            return self.values[item - 1]
+        else:
+            raise IndexError(f"Индекс {item} находится за пределами вектора")
 
 
-p1 = Point(2, 5)
-
-print(p1)
+v = Vector(3, 655, 323, 672, 11, 6)
+print(v[1])  # 3
+print(v[2])  # 655
+print(v["cat"])  # 323
+print(v["park"])  # 672
+# try:
+#     print(v[""])
+# except IndexError as e:
+#     print(e)
