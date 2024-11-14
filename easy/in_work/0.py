40b9c1fbab1e66e36c591d6e6600d1443d35e46a
Alex <santes.au@gmail.com>
2024-11-14 15:41:23 +0300

    815

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,26 +1,2 @@
-class Vector:
-    def __init__(self, *args):
-        self.values = list(args)
-
-    def __repr__(self):
-        return f'Vector({", ".join([str(value) for value in self.values])})'
-
-    def __getitem__(self, item):
-        if isinstance(item, str):
-            item = len(item)
-
-        if 1 <= item <= len(self.values):
-            return self.values[item - 1]
-        else:
-            raise IndexError(f"Индекс {item} находится за пределами вектора")
-
-
-v = Vector(3, 655, 323, 672, 11, 6)
-print(v[1])  # 3
-print(v[2])  # 655
-print(v["cat"])  # 323
-print(v["park"])  # 672
-# try:
-#     print(v[""])
-# except IndexError as e:
-#     print(e)
+x = inpit()
+print(f"{x} {x} {x} {x}")
