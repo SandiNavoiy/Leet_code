61dce54b2bb790e450ed2bfb46f4f31e564d6c78
Alex <santes.au@gmail.com>
2024-11-15 10:59:40 +0300

    815

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,2 +1,42 @@
-x = inpit()
-print(f"{x} {x} {x} {x}")
+class SparseArray:
+    def __init__(self, *args):
+        self._values = list(args)
+
+    @property
+    def values(self):
+        return tuple(self._values)
+
+    def __getitem__(self, number):
+        if number <= len(self._values):
+            return self._values[number]
+        else:
+            self._values.extend([None] * (number - len(self._values)))
+            self._values.append(None)
+
+    def __setitem__(self, number, value=None):
+        if number > len(self._values):
+            self._values.extend([None] * (number - len(self._values)))
+            self._values.insert(number, value)
+        else:
+            self._values[number] = value
+
+    def __delitem__(self, item):
+        if 0 <= int(item) < len(self._values):
+            self._values[int(item)] = None
+
+    def __len__(self):
+        return len(self._values)
+
+
+array = SparseArray()
+print(array.values)
+array[5] = 4
+
+array[0] = 13
+array[10] = 23
+array[5] = 81
+array[7] = 100
+print(array.values)
+print(len(array))
+print(array[20])
+print(array.values)
