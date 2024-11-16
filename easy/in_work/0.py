d33f755845fbe8b75352cb681188bd28f721093b
Sandi_Navoiy <santes.au@gmail.com>
2024-11-16 23:14:19 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,17 +1,10 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self._name
-
-    def set_name(self, value):
-        self._name = value.upper()
-
-    name = property(get_name, set_name)
-
-
-student = Student(name="Kevin")
-print(student.name)
-student.name = "Kev"
-print(student.name)
+#   /~~~\
+#  //^ ^\\
+# (/(_*_)\)
+# _/''*''\_
+# (/_)^(_\)
+print("  /~~~\\")
+print(" //^ ^\\\\")
+print("(/(_*_)\)")
+print("_/''*''\_")
+print("(/_)^(_\)")
