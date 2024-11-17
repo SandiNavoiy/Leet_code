0844fc6327f73e886a0505cbf1f16d3b2c407aa1
Sandi_Navoiy <santes.au@gmail.com>
2024-11-17 17:23:47 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,17 +1,9 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self._name
-
-    def set_name(self, value):
-        self._name = value.upper()
-
-    name = property(get_name, set_name)
 
 
-student = Student(name="Kevin")
-print(student.name)
-student.name = "Kev"
-print(student.name)
+x = "Буря мглою небо кроет"
+x = x.split(" ")
+for i in x:
+    print(i)
+
+
+print(x)
