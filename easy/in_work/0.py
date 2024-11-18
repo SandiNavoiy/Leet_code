2cd49338b29e8f52da251c77337600f80bb4c2e6
Sandi_Navoiy <santes.au@gmail.com>
2024-11-18 23:20:56 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,9 +1,13 @@
-
-
-x = "Буря мглою небо кроет"
-x = x.split(" ")
-for i in x:
-    print(i)
-
-
-print(x)
+a =int(input())
+if a<2:
+    print("Младенец")
+if a>=2 and a<4:
+    print("Малыш")
+elif a>=4 and a<12:
+    print("Ребёнок")
+elif a>=12 and a<19:
+    print("Подросток")
+elif a>=19 and a<65:
+    print("Взрослый человек")
+elif a>=65 and a<1000:
+    print("Пожилой человек")
