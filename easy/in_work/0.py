70704d6cd050fd658465c491a71d918474773f3b
Alex <santes.au@gmail.com>
2024-11-22 14:48:30 +0300

    815

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,13 +1,8 @@
-n = input()
-count = 0
-
-while count <= len(n):
-    if n[count] == "a" or n[count] == "e":
-        print("Ага! Нашлась")
-        break
-    else:
-        print(f"Текущая буква: {n[count]}")
-
-    count += 1
-else:
-    print("Распечатали все буквы")
+n = input().split()
+s = []
+temp = []
+for i in range(len(n)):
+    if n[i].lower() not in temp:
+        temp.append(n[i].lower())
+        s.append(n[i])
+print(*s)
