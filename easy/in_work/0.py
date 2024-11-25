1ae10e5b4bfa440b8fce6d610023a064e2d4fb5b
Sandi_Navoiy <santes.au@gmail.com>
2024-11-25 23:19:29 +0300

    811+

--- easy/in_work/0.py
+++ easy/in_work/0.py
@@ -1,10 +1,6 @@
-def count_AGTC(dna):
-    return dna.count("A"), dna.count("G"), dna.count("T"), dna.count("C")
 
+n = int(input())
 
-# код ниже не стоит удалять, он нужен для проверки
-assert count_AGTC("AGGTC") == (1, 2, 1, 1)
-assert count_AGTC("AAAATTT") == (4, 0, 3, 0)
-assert count_AGTC("AGTTTTT") == (1, 1, 5, 0)
-assert count_AGTC("CCT") == (0, 0, 1, 2)
-print("Проверки пройдены")
+zeroes  = [i for i in range(1,n+1)]
+
+print(zeroes)
