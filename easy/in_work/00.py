a224c7124b554048e9f129f0c754a656fd7ab754
Alex <santes.au@gmail.com>
2024-12-04 14:59:30 +0300

    815

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,5 +1,16 @@
-x = "chase enlarge referee cup offense".lower()
+def unical_word(file_name: str) -> int:
+    with open(f"{file_name}", "r", encoding="utf-8") as f:
+        s = []
 
-for i in x.split():
-    print(i)
-print(all([True if "a" in i else False for i in x.split()]))
+        for i in f:
+            for j in i.split():
+                if j.lower() not in s:
+                    s.append(j.lower())
+
+                else:
+                    continue
+
+    return print(len(s))
+
+
+unical_word("lorem.txt")
