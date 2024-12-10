61cdc85d5b7be0b2d8863d6ee3c6dac1220ef270
Sandi_Navoiy <santes.au@gmail.com>
2024-12-10 23:01:51 +0300

    811+

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,7 +1,28 @@
 from datetime import datetime
 
-first_flight = datetime(1961, 4, 12, 9, 7, 0)
+def is_third_tuesday(dt_str: str) -> bool:
+    dt = datetime.strptime(dt_str, '%b %d, %Y')
+    dt = dt.date()
 
 
-print(first_flight)
-# 1961-04-12 09:07:00
+    if dt.isoweekday() ==2 and 14 <= dt.day <= 21:
+        return  True
+    else:
+        return False
+
+
+
+
+
+assert is_third_tuesday("Sep 19, 2023")
+assert not is_third_tuesday("Sep 12, 2023")
+assert is_third_tuesday("Jul 18, 2023")
+assert not is_third_tuesday("Jul 11, 2023")
+assert not is_third_tuesday('Jun 23, 2015')
+assert is_third_tuesday('Jun 16, 2015')
+assert is_third_tuesday('Jul 21, 2015')
+assert is_third_tuesday("Oct 20, 2015")
+assert is_third_tuesday("Nov 17, 2015")
+
+#
+# print('Good')
