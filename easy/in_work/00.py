0d656d368e50f4d4d4c6a9fbcb9eacc2295898ee
Sandi_Navoiy <santes.au@gmail.com>
2024-12-11 23:07:38 +0300

    811+

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,28 +1,11 @@
-from datetime import datetime
-
-def is_third_tuesday(dt_str: str) -> bool:
-    dt = datetime.strptime(dt_str, '%b %d, %Y')
-    dt = dt.date()
-
-
-    if dt.isoweekday() ==2 and 14 <= dt.day <= 21:
-        return  True
+code = input()
+n = int(input())
+if (code == "sq" or code == "tr") and 1 <= n <= 9:
+    if code == "sq":
+        for i in range(n):
+            print("#"*2*n)
     else:
-        return False
-
-
-
-
-
-assert is_third_tuesday("Sep 19, 2023")
-assert not is_third_tuesday("Sep 12, 2023")
-assert is_third_tuesday("Jul 18, 2023")
-assert not is_third_tuesday("Jul 11, 2023")
-assert not is_third_tuesday('Jun 23, 2015')
-assert is_third_tuesday('Jun 16, 2015')
-assert is_third_tuesday('Jul 21, 2015')
-assert is_third_tuesday("Oct 20, 2015")
-assert is_third_tuesday("Nov 17, 2015")
-
-#
-# print('Good')
+        for i in range(n):
+            print(" "*(n-i) + "#"*(2*i+1))
+else:
+    print("WRONG_DATA")
