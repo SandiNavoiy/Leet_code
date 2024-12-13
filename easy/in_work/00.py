e2e4cd7e10da429854480899bee7ef74142de1c4
Alex <santes.au@gmail.com>
2024-12-13 14:45:42 +0300

    815

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,69 +1,55 @@
-# Напишите определение классов Product и ShoppingCart
-
-class Product:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
+from datetime import date
 
 
-apple = Product("Apple", 1.5)
-banana = Product("Banana", 0.75)
-chocolate = Product("Chocolate", 2.99)
-
-
-class ShoppingCart:
-    def __init__(self):
-        self._items = []
-
-    def add_item(self, item):
-
-        self._items.append(item)
-        self._items = sorted(self._items, key=lambda x: (x.price, x.name))
-
-    def remove_item(self, item):
-        if item in self._items:
-            self._items.remove(item)
-        else:
-            raise ValueError("Item not found in the cart")
-    def __len__(self):
-        return len(self._items)
-
-    @property
-    def items(self):
-
-
-        return self._items
-
-    @property
-    def total_price(self):
-        return sum(item.price for item in self._items)
-
-
-cart = ShoppingCart()
-cart.add_item(banana)
-cart.add_item(apple)
-cart.add_item(chocolate)
-
-assert len(cart) == 3
-
-assert cart.items == [banana, apple, chocolate]
-#
-milk = Product('Milk', 1)
-cart.add_item(milk)
-
-assert cart.items == [banana, milk, apple, chocolate]
-print("Items in the cart:", [item.name for item in cart.items])
-print("Total price:", cart.total_price)
-assert cart.total_price == 6.24
-cart.add_item(banana)
-assert len(cart) == 5
-assert cart.total_price == 6.99
-cart.remove_item(milk)
-assert len(cart) == 4
-cart.remove_item(banana)
-cart.remove_item(banana)
-assert len(cart) == 2
-try:
-    cart.remove_item(banana)
-except ValueError:
+def get_dates_until_end_of_month(dt: date) -> list[date]:
     pass
+
+
+assert get_dates_until_end_of_month(date(2023, 12, 28)) == [
+    date(2023, 12, 28),
+    date(2023, 12, 29),
+    date(2023, 12, 30),
+    date(2023, 12, 31),
+]
+assert get_dates_until_end_of_month(date(2024, 2, 23)) == [
+    date(2024, 2, 23),
+    date(2024, 2, 24),
+    date(2024, 2, 25),
+    date(2024, 2, 26),
+    date(2024, 2, 27),
+    date(2024, 2, 28),
+    date(2024, 2, 29),
+]
+
+assert get_dates_until_end_of_month(date(2023, 2, 25)) == [
+    date(2023, 2, 25),
+    date(2023, 2, 26),
+    date(2023, 2, 27),
+    date(2023, 2, 28),
+]
+
+assert get_dates_until_end_of_month(date(2022, 5, 10)) == [
+    date(2022, 5, 10),
+    date(2022, 5, 11),
+    date(2022, 5, 12),
+    date(2022, 5, 13),
+    date(2022, 5, 14),
+    date(2022, 5, 15),
+    date(2022, 5, 16),
+    date(2022, 5, 17),
+    date(2022, 5, 18),
+    date(2022, 5, 19),
+    date(2022, 5, 20),
+    date(2022, 5, 21),
+    date(2022, 5, 22),
+    date(2022, 5, 23),
+    date(2022, 5, 24),
+    date(2022, 5, 25),
+    date(2022, 5, 26),
+    date(2022, 5, 27),
+    date(2022, 5, 28),
+    date(2022, 5, 29),
+    date(2022, 5, 30),
+    date(2022, 5, 31),
+]
+print("Good")
