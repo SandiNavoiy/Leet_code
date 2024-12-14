7f68add87ab39172f44b5407d93da58f746dffc6
Sandi_Navoiy <santes.au@gmail.com>
2024-12-14 22:54:16 +0300

    811+

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,55 +1,12 @@
-from datetime import date
+from datetime import datetime, timedelta
+n= input()
+dt = datetime.strptime(n, '%d-%m-%Y')
+dt = dt.date()
+print('Payment Schedule:')
+for i in range(12):
+
+    print(f"Payment {i+1}: {dt.strftime('%Y-%d-%m-%Y')}")
+    dt = dt + timedelta(days=28)
 
 
-def get_dates_until_end_of_month(dt: date) -> list[date]:
-    pass
 
-
-assert get_dates_until_end_of_month(date(2023, 12, 28)) == [
-    date(2023, 12, 28),
-    date(2023, 12, 29),
-    date(2023, 12, 30),
-    date(2023, 12, 31),
-]
-assert get_dates_until_end_of_month(date(2024, 2, 23)) == [
-    date(2024, 2, 23),
-    date(2024, 2, 24),
-    date(2024, 2, 25),
-    date(2024, 2, 26),
-    date(2024, 2, 27),
-    date(2024, 2, 28),
-    date(2024, 2, 29),
-]
-
-assert get_dates_until_end_of_month(date(2023, 2, 25)) == [
-    date(2023, 2, 25),
-    date(2023, 2, 26),
-    date(2023, 2, 27),
-    date(2023, 2, 28),
-]
-
-assert get_dates_until_end_of_month(date(2022, 5, 10)) == [
-    date(2022, 5, 10),
-    date(2022, 5, 11),
-    date(2022, 5, 12),
-    date(2022, 5, 13),
-    date(2022, 5, 14),
-    date(2022, 5, 15),
-    date(2022, 5, 16),
-    date(2022, 5, 17),
-    date(2022, 5, 18),
-    date(2022, 5, 19),
-    date(2022, 5, 20),
-    date(2022, 5, 21),
-    date(2022, 5, 22),
-    date(2022, 5, 23),
-    date(2022, 5, 24),
-    date(2022, 5, 25),
-    date(2022, 5, 26),
-    date(2022, 5, 27),
-    date(2022, 5, 28),
-    date(2022, 5, 29),
-    date(2022, 5, 30),
-    date(2022, 5, 31),
-]
-print("Good")
