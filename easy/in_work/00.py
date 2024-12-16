7c0062f6aff7d75283a8f35b5d8570fded605d64
Alex <santes.au@gmail.com>
2024-12-16 15:04:15 +0300

    815

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,12 +1,53 @@
-from datetime import datetime, timedelta
-n= input()
-dt = datetime.strptime(n, '%d-%m-%Y')
-dt = dt.date()
-print('Payment Schedule:')
-for i in range(12):
+from collections import UserDict
 
-    print(f"Payment {i+1}: {dt.strftime('%Y-%d-%m-%Y')}")
-    dt = dt + timedelta(days=28)
+# Напишите определение класса ContactBook
 
 
+class ContactBook:
+    def __init__(self):
+        self.data = UserDict()
 
+    def add_contact(self, k, v):
+        if k in self.data:
+            print(f"Контакт {k} уже существует")
+        else:
+            self.data[k] = v
+            print(f"Контакт {k} создан")
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def display_contacts(self):
+        for k, v in self.data.items():
+            print(f"{k}: {v}")
+
+    def update_contact(self, k, v):
+        if k in self.data:
+            self.data[k] = v
+
+        else:
+            print(f"Контакт {k} не найден")
+
+    def delete_contact(self, k):
+        if k in self.data:
+            del self.data[k]
+            print(f"Контакт {k} удален")
+        else:
+            print(f"Контакт {k} не найден")
+
+
+contact_book = ContactBook()
+contact_book.add_contact("John", "123-456-7890")
+assert contact_book.data["John"] == "123-456-7890"
+contact_book.add_contact("John", "345-666")  # Печатает Контакт John уже существует.
+contact_book.add_contact("Alice", "987-654-3210")
+assert contact_book.data["Alice"] == "987-654-3210"
+contact_book.display_contacts()
+#
+contact_book.update_contact("John", "111-222-3333")
+assert contact_book.data["John"] == "111-222-3333"
+contact_book.display_contacts()
+
+contact_book.delete_contact("Alice")
+assert "Alice" not in contact_book.data
+contact_book.display_contacts()
