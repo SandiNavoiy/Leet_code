d2bb774977053c73678fdd4393df1b2a674098d9
Sandi_Navoiy <santes.au@gmail.com>
2024-12-16 23:09:41 +0300

    811+

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,53 +1,34 @@
-from collections import UserDict
+from collections import UserString
 
-# Напишите определение класса ContactBook
-
-
-class ContactBook:
-    def __init__(self):
-        self.data = UserDict()
-
-    def add_contact(self, k, v):
-        if k in self.data:
-            print(f"Контакт {k} уже существует")
-        else:
-            self.data[k] = v
-            print(f"Контакт {k} создан")
-
-    def __getitem__(self, item):
-        return self.data[item]
-
-    def display_contacts(self):
-        for k, v in self.data.items():
-            print(f"{k}: {v}")
-
-    def update_contact(self, k, v):
-        if k in self.data:
-            self.data[k] = v
-
-        else:
-            print(f"Контакт {k} не найден")
-
-    def delete_contact(self, k):
-        if k in self.data:
-            del self.data[k]
-            print(f"Контакт {k} удален")
-        else:
-            print(f"Контакт {k} не найден")
+# Напишите определение класса StringWithSort
 
 
-contact_book = ContactBook()
-contact_book.add_contact("John", "123-456-7890")
-assert contact_book.data["John"] == "123-456-7890"
-contact_book.add_contact("John", "345-666")  # Печатает Контакт John уже существует.
-contact_book.add_contact("Alice", "987-654-3210")
-assert contact_book.data["Alice"] == "987-654-3210"
-contact_book.display_contacts()
+# Проверки для класса StringWithSort
+
+# Пример использования:
+class StringWithSort:
+    def __init__(self, s: str):
+        self.data = UserString(s)
+
+    def sort(self, key=None, reverse=False):
+        temp = sorted(str(self.data), key=key, reverse=reverse)
+        # Соединяем отсортированные символы в строку
+        return ''.join(temp)
+
+
+
+
+
+
+s = StringWithSort("Golden retriver")
+
+assert s.sort() == ' Gdeeeilnorrrtv'
+assert s.data == 'Golden retriver'
+
+assert s.sort(reverse=True) == 'vtrrronlieeedG '
 #
-contact_book.update_contact("John", "111-222-3333")
-assert contact_book.data["John"] == "111-222-3333"
-contact_book.display_contacts()
-
-contact_book.delete_contact("Alice")
-assert "Alice" not in contact_book.data
-contact_book.display_contacts()
+new_s = StringWithSort('HelloMyFriend')
+# # При обычной сортироки сперва идут заглавные буквы потом строчные
+assert new_s.sort() == 'FHMdeeillnory'
+# # Сортировка с ключом lower, который во время сравнения все буквы делает строчными
+assert new_s.sort(key=str.lower) == 'deeFHillMnory'
