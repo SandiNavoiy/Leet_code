a772ebf20521949233e54e5fa9ed3af6689781e7
Alex <santes.au@gmail.com>
2024-12-17 12:45:36 +0300

    815

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,48 +1,38 @@
 from collections import Counter
+from typing import Optional
 
 
-def find_difference_with_counter(lst1: list, lst2: list) -> list:
-    c1 = Counter(lst1)
-    c2 = Counter(lst2)
-    rez = []
-    for i in c1 - c2:
-        rez.append(i)
-    rez.sort()
-    return rez
+def find_majority_element(nums: list) -> Optional[int]:
+    c = Counter(nums)
+
+    if len(c) == 0:
+        return None
+    elif len(c) == 1:
+        return c.most_common(1)[0][0]
+    x = c.most_common(1)
+    if x[0][1] > 1:
+        return x[0][0]
+    else:
+        return None
 
 
-print(find_difference_with_counter([1, 1, 2, 3, 3, 4, 4, 5, 6, 7], [1, 1, 2, 4, 5, 6]))
-assert find_difference_with_counter([1, 2, 2, 3, 4, 4, 5], [2, 3, 3, 4, 5, 6]) == [
-    1,
-    2,
-    4,
-]
+print(find_majority_element([7]))
+assert find_majority_element([3, 8, 7, 3, 3, 5, 3, 5, 1, 3, 3]) == 3
 #
-assert find_difference_with_counter([5, 4, 5, 1, 2, 7, 3], [2, 3, 3, 4, 5, 6]) == [
-    1,
-    5,
-    7,
-]
-
-assert find_difference_with_counter(
-    [1, 1, 2, 3, 3, 4, 4, 5, 6, 7], [1, 1, 2, 4, 5, 6]
-) == [3, 3, 4, 7]
-
-# assert find_difference_with_counter(
-#     [1, 1, 1, 1],
-#     [
-#         1,
-#         1,
-#     ],
-# ) == [1, 1]
-
-# assert (
-#     find_difference_with_counter(
-#         [
-#             1,
-#             1,
-#         ],
-#         [1, 1, 1, 1],
-#     )
-#     == []
-# )
+# # Тест 2: Элемент большинства присутствует
+assert find_majority_element([6, 8, 4, 6, 8, 6, 6]) == 6
+#
+# # Тест 3: Элемент большинства отсутствует
+assert find_majority_element([1, 2, 3]) is None
+#
+# # Тест 4: Пустой список
+assert find_majority_element([]) is None
+#
+# # Тест 5: Список с одним элементом
+# assert find_majority_element([7]) == 7
+#
+# # Тест 6: Список с двумя элементами
+assert find_majority_element([7, 7]) == 7
+#
+# # Тест 7: Список с тремя элементами (элемент большинства отсутствует)
+assert find_majority_element([7, 8, 9]) is None
