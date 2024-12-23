cb673ad5e41e04eea8a071435846bfa7627f4e24
Alex <santes.au@gmail.com>
2024-12-23 14:11:05 +0300

    815

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,29 +1,24 @@
 import os
-from datetime import time
-
-
-import os
 import time
 
 path = "."
-files_lst = os.listdir(path)
-print(files_lst)
-idxs = list()
+x = os.listdir(path)
+file = [i for i in x if os.path.isfile(i)]
+dir = [i for i in x if os.path.isdir(i)]
 
-for idx in range(len(files_lst)):
-    print(idx)
-    name = os.path.split(files_lst[idx])[1]
+print(file)
+print(dir)
 
-    if os.path.isfile(os.path.join(path, name)):
-        idxs.append(idx)
+for i in file:
+    print("File         :", i)
+    print("Access time  :", time.ctime(os.path.getatime(i)))
+    print("Modified time:", time.ctime(os.path.getmtime(i)))
+    print("Change time  :", time.ctime(os.path.getctime(i)))
+    print("Size         :", os.path.getsize(i))
 
-print(idxs)
-for indx in range(len(idxs)):
-    print("File         :", files_lst[indx])
-    print("Access time  :", time.ctime(os.path.getatime(files_lst[indx])))
-    print("Modified time:", time.ctime(os.path.getmtime(files_lst[indx])))
-    print("Change time  :", time.ctime(os.path.getctime(files_lst[indx])))
-    print("Size         :", os.path.getsize(files_lst[indx]))
-    if indx == len(idxs) - 1:
-        break
-    print("---------------------------------------")
+for i in dir:
+    print("dir         :", i)
+    print("Access time  :", time.ctime(os.path.getatime(i)))
+    print("Modified time:", time.ctime(os.path.getmtime(i)))
+    print("Change time  :", time.ctime(os.path.getctime(i)))
+    print("Size         :", os.path.getsize(i))
