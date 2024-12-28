a370430404ba3b5b9c3d8d3c651ecb489d23d8ea
Sandi_Navoiy <santes.au@gmail.com>
2024-12-29 01:19:05 +0300

    821

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,25 +1,26 @@
-import zipfile
-import os
+from pprint import pprint
 
-# Путь к архиву
-zip_path = "secrets.zip"
+data = {
+    "users": [
+        {
+            "userId": 1,
+            "firstName": "Krish",
+            "lastName": "Lee",
+            "emailAddress": "krish.lee@example.com"
+        },
+        {
+            "userId": 2,
+            "firstName": "racks",
+            "lastName": "jacson",
+            "emailAddress": "racks.jacson@example.com"
+        },
+        {
+            "userId": 3,
+            "firstName": "denial",
+            "lastName": "roast",
+            "emailAddress": "denial.roast@example.com"
+        }
+    ]
+}
 
-def calculate_json_size(zip_path):
-    total_size = 0
-
-    # Открываем архив
-    with zipfile.ZipFile(zip_path, 'r') as archive:
-        # Получаем список всех файлов в архиве
-        for file_info in archive.infolist():
-            # Проверяем расширение файла
-            if file_info.filename.endswith('.json'):
-                total_size += file_info.file_size
-
-    return total_size
-
-
-if os.path.exists(zip_path):
-    json_size = calculate_json_size(zip_path)
-    print(f"Суммарный размер файлов .json: {json_size} байт")
-else:
-    print(f"Архив {zip_path} не найден.")
+pprint(data)
