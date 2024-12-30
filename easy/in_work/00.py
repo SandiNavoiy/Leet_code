656f202a2ecafeea8fa269e2cf3daeb0d3d1356c
Sandi_Navoiy <santes.au@gmail.com>
2024-12-31 00:17:36 +0300

    821

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,11 +1,10 @@
-import itertools
+import calendar
+a = int(input())
+b = int(input())
 
-x = 17
-y = 32684323
-rez = [chr(i + 64) for i in range(1, x+1)]
-count = 1
-for i in itertools.permutations(rez):
-    if count == y:
-        print(' '.join(map(str, i)))
-        break
-    count = count + 1
+
+import calendar
+
+print(calendar.monthrange(a, b)[1])
+
+
