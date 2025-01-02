2da14ecd28bd6eb3810826fd63a9ce1cb6634d45
Sandi_Navoiy <santes.au@gmail.com>
2025-01-02 23:41:58 +0300

    821

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,51 +1,27 @@
-import unittest
+import math
+from unittest.mock import Mock
+# Работа функции math до создания mock
+print(math.sqrt(9))
+print(math.sqrt(100))
 
+try:
+    print(math.sqrt())
+except TypeError as ex:
+    print(ex)
 
-class Book:
-    def __init__(self, title, author, pages, price):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
+try:
+    print(math.sqrt(5, 7))
+except TypeError as ex:
+    print(ex)
 
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+# -----------------------------------------------------
+# Ниже между двумя комментариями замокайте функцию math
 
-    def get_reading_time(self):
-        return f"{self.pages * 1.5} minutes"
+math.sqrt = Mock()
+math.sqrt.return_value = 'Квадратный корень'
+# Проверяем вызов
+# ---------------
 
-    def apply_discount(self, discount):
-        if not isinstance(discount, float):
-            raise ValueError('Discount must be float number')
-        if 0 <= discount <= 1:
-            discounted_price = self.price - (discount * self.price)
-            return f"${discounted_price}"
-        raise ValueError('Discount must be float number between 0 and 1')
-
-
-class TestBookClass(unittest.TestCase):
-    def test_init_method(self):
-        with self.assertRaises(TypeError):
-            Book()
-
-    def test__str__method(self):
-        r = Book(1, 2, 3, 4)
-
-
-        self.assertEqual(print(r), "1 by 2")
-
-
-    # def test_get_reading_time(self):
-    #     pass
-    #
-    # def test_apply_discount_not_float(self):
-    #     pass
-    #
-    # def test_apply_discount_more_than_1(self):
-    #     pass
-    #
-    # def test_apply_discount_less_than_0(self):
-    #     pass
-    #
-    # def test_apply_discount_good_case(self):
-    #     pass
+print(math.sqrt(2))
+print(math.sqrt(2, 5))
+print(math.sqrt())
