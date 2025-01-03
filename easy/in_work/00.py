639ea4f177c39023800df7e027bdd09f899c77e1
Sandi_Navoiy <santes.au@gmail.com>
2025-01-03 23:37:56 +0300

    821

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,27 +1,4 @@
-import math
-from unittest.mock import Mock
-# Работа функции math до создания mock
-print(math.sqrt(9))
-print(math.sqrt(100))
-
-try:
-    print(math.sqrt())
-except TypeError as ex:
-    print(ex)
-
-try:
-    print(math.sqrt(5, 7))
-except TypeError as ex:
-    print(ex)
-
-# -----------------------------------------------------
-# Ниже между двумя комментариями замокайте функцию math
-
-math.sqrt = Mock()
-math.sqrt.return_value = 'Квадратный корень'
-# Проверяем вызов
-# ---------------
-
-print(math.sqrt(2))
-print(math.sqrt(2, 5))
-print(math.sqrt())
+if "walrus" in (n:= input()):
+    print(f"Нашли моржа")
+else:
+    print(f"Никаких моржей тут нет")
