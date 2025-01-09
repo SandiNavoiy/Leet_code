74d76bcb526d4041b6e2424dc5ceb16de0795c43
Alex <santes.au@gmail.com>
2025-01-09 09:52:42 +0300

    815

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,4 +1,15 @@
-if (x := input()).isdigit():
-    print("Можно преобразовать")
-else:
-    print("Нельзя преобразовать")
+# pass = input()
+pas = "qwerty"
+n = 3
+while True:
+    new = input()
+    n -= 1
+    if new != pas:
+        print("Введен неправильный пароль!")
+        print(f"Лимит попыток = {n}")
+    else:
+        print("Вы ввели правильный пароль! Добро пожаловать!")
+        break
+    if n == 0:
+        print("Вы потратили все свои попытки! Приходите завтра")
+        break
