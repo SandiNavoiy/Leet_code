aeb7d2d52388a59f048011c0bac79d62fa375881
Sandi_Navoiy <santes.au@gmail.com>
2025-01-09 23:18:12 +0300

    821

--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,15 +1,7 @@
-# pass = input()
-pas = "qwerty"
-n = 3
-while True:
-    new = input()
-    n -= 1
-    if new != pas:
-        print("Введен неправильный пароль!")
-        print(f"Лимит попыток = {n}")
-    else:
-        print("Вы ввели правильный пароль! Добро пожаловать!")
-        break
-    if n == 0:
-        print("Вы потратили все свои попытки! Приходите завтра")
-        break
+x = 0
+for i in range(1, 101):
+    if i%2 != 0 and i%3 != 0 and i%5 != 0:
+        x = x + 1
+
+
+print(x)
