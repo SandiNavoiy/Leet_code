cbba21f24d3a8be9128ce7c8c33462c6d682407e
Sandi_Navoiy <santes.au@gmail.com>
2025-01-10 19:31:36 +0300

    822

---
+++ easy/3364.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumSumSubarray(self, nums: list[int], l: int, r: int) -> int:
+        ''''''
+
+        ss= 10000000000000
+        flag = False
+
+
+        for i in range(len(nums)):
+            for j in range(l, r+1):
+
+
+                if sum(nums[i:i+j])>0 and len(nums[i:i+j]) >=l:
+                    ss = min(ss, sum(nums[i:i+j]))
+                    flag = True
+
+        if flag == False:
+            return -1
+        return ss
+nn = [-2, 2, -3, 1]
+l = 2
+r = 3
+
+s = Solution()
+print(s.minimumSumSubarray(nn, l, r))
