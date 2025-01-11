bbd043efe8a88bc9ecc475350134a87859db0a14
Sandi_Navoiy <santes.au@gmail.com>
2025-01-12 00:45:04 +0300

    824

---
+++ easy/1566.py
@@ -0,0 +1,17 @@
+class Solution:
+    def containsPattern(self, arr: list[int], m: int, k: int) -> bool:
+        i = 0
+        while i <= len(arr) - 1:
+            p = arr[i:i + m]
+            if p * k == arr[i:i + m * k]:
+                return True
+
+            i += 1
+
+        return False
+
+arr = [1,2,1,2,1,1,1,3]
+m = 2
+k = 2
+s  = Solution()
+print(s.containsPattern(arr, m, k))
--- easy/in_work/00.py
+++ easy/in_work/00.py
@@ -1,19 +1,3 @@
-import pandas as pd
+arr = [1,2,1,2,1,1,1,3]
 
-gdp_usa = [
-    31837000000,
-    643000000,
-    46607000000,
-    42078000000,
-    19325000000,
-]
-period = [
-    "2018",
-    "2019",
-    "2020",
-    "2021",
-    "2022",
-]
-s = pd.Series(data=gdp_usa, index=period)
-
-print(s)
+print(arr.count(1))
