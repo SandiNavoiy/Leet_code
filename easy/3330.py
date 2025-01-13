cd15b112f9e4b25ccf4b95d3ad1267838388a92c
Alex <santes.au@gmail.com>
2025-01-13 10:37:22 +0300

    825

---
+++ easy/3330.py
@@ -0,0 +1,14 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        count = 0
+        prev = ""
+        for c in word:
+            if c == prev:
+                count += 1
+            prev = c
+        return count + 1
+
+
+word = "abbcccc"
+s = Solution()
+print(s.possibleStringCount(word))
