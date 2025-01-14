307e7a9de5e16e781e6655d42b574d923d7655a9
Sandi_Navoiy <santes.au@gmail.com>
2025-01-14 23:04:53 +0300

    826+

---
+++ easy/in_work/3407.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hasMatch(self, s: str, p: str) -> bool:
+        ''''''
+        x  = p.split('*')
+        first = s.find(x[0])
+        if first == -1:
+            return False
+        second = s.rfind(x[1])
+        if second == -1:
+            return False
+        if second > first:
+            return True
+        else:
+            return False
+
+
+s = "leetcode"
+p = "ee*e"
+sol = Solution()
+print(sol.hasMatch(s, p))
