b07c1569050483fc1836be5c84e0b1c600af4d5f
Sandi_Navoiy <santes.au@gmail.com>
2025-01-14 22:46:07 +0300

    826

---
+++ easy/3360.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canAliceWin(self, n: int) -> bool:
+        ''''''
+        if n < 10:
+            return False
+        i = 10
+        step = 0
+        while n>=i:
+            n = n -i
+            i -= 1
+            step += 1
+        if step % 2 != 0:
+            return True
+        else:
+            return False
+
+
+n = 19
+s  = Solution()
+print(s.canAliceWin(n))
