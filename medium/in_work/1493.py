694c6803fb9b6d0356dcf33f819fed11a4db6043
Sandi_Navoiy <santes.au@gmail.com>
2025-03-24 23:10:16 +0300

    839+

---
+++ medium/in_work/1493.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestSubarray(self, nums: list[int]) -> int:
+        ''''''
+
+        rez = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)+1):
+                if nums[i:j].count(0)<=1:
+
+                    rez = max(rez, len(nums[i:j])-1)
+                else:
+                    break
+        return  rez
+nums = [0,1,1,1,0,1,1,0,1]
+
+
+s = Solution()
+print(s.longestSubarray(nums))
