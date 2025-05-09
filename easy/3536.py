c9898ded87879521f357cb95c560382a9d5db3af
Sandi_Navoiy <santes.au@gmail.com>
2025-05-09 22:55:37 +0300

    842

---
+++ easy/3536.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        """Максимальное произведение двух цифр"""
+
+        arr = sorted([int(i) for i in str(n)])
+
+
+        return arr[-1] * arr[-2]
+
+
+s = Solution()
+print(s.maxProduct(124))
