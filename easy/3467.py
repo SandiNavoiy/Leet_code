0e954fc22bf05966196f32f4820bc266c11befc3
Sandi_Navoiy <santes.au@gmail.com>
2025-05-09 22:50:05 +0300

    841

---
+++ easy/3467.py
@@ -0,0 +1,18 @@
+class Solution:
+    def transformArray(self, nums: list[int]) -> list[int]:
+        """Преобразование массива по четности"""
+        chet= 0
+        nechet  =0
+
+        for i in nums:
+            if i %2 == 0:
+                chet += 1
+
+            else:
+                nechet += 1
+        return [0] * chet + [1]*nechet
+
+
+
+s = Solution()
+print(s.transformArray([4,3,2,1]))
