4a4ffc3ba29d8416e87c33a5bbc9a90b6d4b9284
Sandi_Navoiy <santes.au@gmail.com>
2025-05-09 22:40:58 +0300

    840

---
+++ easy/1346.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkIfExist(self, arr: list[int]) -> bool:
+
+
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if (i != j) and (arr[i] == 2 * arr[j]):
+                    return True
+        return False
+
+
+s = Solution()
+print(s.checkIfExist(([10,2,5,3])))
