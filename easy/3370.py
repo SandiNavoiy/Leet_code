fadcf33afec6031565c02356db34ebbd34bdba79
Sandi_Navoiy <santes.au@gmail.com>
2025-05-10 19:18:14 +0300

    843

---
+++ easy/3370.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        """Наименьшее число со всеми установленными битами"""
+        while True:
+            if "0" not in bin(n)[2:]:
+                return n
+            else:
+                n +=1
+
+
+
+s = Solution()
+print(s.smallestNumber(10))
