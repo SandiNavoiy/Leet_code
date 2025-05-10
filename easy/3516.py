12c1cec9d9b1db397a31e9c29fc6024f02ec9ed0
Sandi_Navoiy <santes.au@gmail.com>
2025-05-10 22:57:19 +0300

    848

---
+++ easy/3516.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        """Найти ближайшего человека"""
+
+        if abs(z-x) < abs(z-y):
+            return 1
+        elif abs(z-x) > abs(z-y):
+            return 2
+        else:
+            return 0
+
+
+s = Solution()
+print(s.findClosest(x = 2, y = 7, z = 4))
