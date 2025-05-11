e5d0061248b585d058dd69f4fab27e032f4d2743
Sandi_Navoiy <santes.au@gmail.com>
2025-05-11 20:36:19 +0300

    854

---
+++ easy/2682.py
@@ -0,0 +1,22 @@
+class Solution:
+    def circularGameLosers(self, n: int, k: int) -> list[int]:
+        '''Найдите проигравших в круговой игреь'''
+
+        arr = [i for i in range(1, n+1)]
+        s  =set()
+        i = 0
+        step = 0
+        while True:
+            if arr[step] in s:
+                return sorted(set(arr) - s)
+            else:
+                s.add(arr[step])
+                i +=1
+                step = ((step + i*k) % len(arr))
+
+
+
+
+
+s = Solution()
+print(s.circularGameLosers(n = 4, k = 4))
