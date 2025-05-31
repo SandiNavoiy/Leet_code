60a1a4c193213b136b6e0c453a0a602ce06dfe90
Sandi_Navoiy <santes.au@gmail.com>
2025-05-31 15:33:14 +0300

    859

---
+++ medium/62.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        """Уникальные пути"""
+        #решаем методами динамического програмирования
+        rez = [[1]*n for _ in range(m)]
+
+
+        for i in range(1, m):
+            for j in range(1, n):
+                rez[i][j] = rez[i][j-1] + rez[i-1][j]
+
+
+        print(rez)
+        return rez[m-1][n-1]
+
+
+s = Solution()
+print(s.uniquePaths(1,1))
