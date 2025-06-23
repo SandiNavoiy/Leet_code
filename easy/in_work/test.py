37357cf0834a41b37b3acdde8ed9cec8e2e8630b
Sandi_Navoiy <santes.au@gmail.com>
2025-06-23 12:27:06 +0300

    866+

--- easy/in_work/test.py
+++ easy/in_work/test.py
@@ -1,27 +1,8 @@
-import unittest
+import pandas as pd
+import openpyxl
 
-
-class Book:
-    def __init__(self, title, author, pages, price):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
-
-    def __str__(self):
-        return f"{self.title} by {self.author}"
-
-    def get_reading_time(self):
-        return f"{self.pages * 1.5} minutes"
-
-    def apply_discount(self, discount):
-        if not isinstance(discount, float):
-            raise ValueError("Discount must be float number")
-        if 0 <= discount <= 1:
-            discounted_price = self.price - (discount * self.price)
-            return f"${discounted_price}"
-        raise ValueError("Discount must be float number between 0 and 1")
-
-
-r = Book(1, 2, 3, 4)
-print(r)
+print("openpyxl is working")
+# df1 = pd.read_excel("USD000UTSTOM_170101_171231.xlsx")
+#
+#
+# print(df1)
