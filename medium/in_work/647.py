4ab919c8f9687ad0a8e3c2d876d6692ab0d8f206
Sandi_Navoiy <santes.au@gmail.com>
2025-08-18 19:46:06 +0300

    866+

---
+++ medium/in_work/647.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        """Палиндромные подстроки"""
+        count = 0
+        def is_palindrome(s, left, right):
+
+
+
+        return count
+
+
+s = Solution()
+s.countSubstrings("abc")
