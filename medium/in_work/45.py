e44e9e7d5c11f5921de79737a003d8ebda48aabe
Alex <santes.au@gmail.com>
2025-09-05 13:19:41 +0300

    869

--- medium/in_work/45.py
+++ medium/in_work/45.py
@@ -1,6 +1,16 @@
 class Solution:
     def jump(self, nums: list[int]) -> int:
         """"""
+        end = 0
+        step = 0
+
+        count = 0
+        for i in range(len(nums) - 1):
+            step = max(step, i + nums[i])
+            if i == end:
+                end = step
+                count += 1
+        return count
 
 
 s = Solution()
