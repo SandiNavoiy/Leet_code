ecc55ec928bc04b6ea6a836b1b8018ee037affee
Alex <santes.au@gmail.com>
2026-03-10 11:13:07 +0300

    906

---
+++ medium/11.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        """Контейнер с большим количеством воды"""
+
+        left = 0
+        right = len(height) - 1
+        sum = min(height[left], height[right]) * (right - left)
+        while left < right:
+            if height[left] < height[right]:
+                left = left + 1
+            else:
+                right = right - 1
+            sum = max(sum, min(height[left], height[right]) * (right - left))
+
+        return sum
+
+
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+
+s = Solution()
+print(s.maxArea(height))
+# 49
