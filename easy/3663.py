fe5f50f4f0ce397a365433b1e403bd4bf8e9ef1e
Sandi_Navoiy <santes.au@gmail.com>
2026-03-22 13:59:34 +0300

    917

--- easy/3663.py
+++ easy/3663.py
@@ -1,15 +1,19 @@
-from collections import Counter
-
-
 class Solution:
-    def getLeastFrequentDigit(self, n: int) -> int:
-        """Найдите наименее часто встречающуюся цифру."""
-        #находим частоты
-        d = dict(Counter(str(n)))
-        # сортируем сначала по частоте, потом по цифре
-        d = sorted(d.items(), key=lambda x: (x[1], x[0]))
-        #выводим первый результат
-        return int(d[0][0])
+    def largestEven(self, s: str) -> str:
+        """Напишите функцию, которая принимает строку и возвращает наибольшее четное число, которое можно получить из цифр строки."""
+
+        if s[-1] == "2":
+            return s
+
+        else:
+            inx  = len(s) - 1
+            while inx >= 0:
+                if s[inx] == "2":
+                    return s[:inx] + "2"
+                inx -= 1
+
+        return ""
+
 
 s = Solution()
-print(s.getLeastFrequentDigit(1553322))
+print(s.largestEven("111"))
